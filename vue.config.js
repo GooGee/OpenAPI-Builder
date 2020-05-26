2c9f7e59d621824e6927ef1519c854625b3bb3cf
GooGee <mou-gj@live.com>
2020-05-26 13:29:32 +0800

    + title version

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const Title = 'OpenAPI-Builder'
+
+process.env.VUE_APP_TITLE = Title
+process.env.VUE_APP_VERSION = require('./package.json').version
+
 module.exports = {
     chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = Title
+            return args
+        })
         config.resolve.extensions.merge(['.ts']).end()
         config.module.rules.delete('eslint').end()
         config.module
