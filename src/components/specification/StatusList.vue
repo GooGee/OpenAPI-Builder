c9a8aab0c3cf48ddc00e243cbee64f90c842f8a2
GooGee <mou-gj@live.com>
2020-05-29 15:21:51 +0800

    + StatusList

---
+++ src/components/specification/StatusList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div>
+            <h2 class="inline mr11px">Responses</h2>
+            <AddButton :manager="manager" name="name"></AddButton>
+        </div>
+
+        <div v-for="item in manager.list" :key="item.name" class="mtb11px">
+            <b-button-group>
+                <DeleteButton :manager="manager" :item="item"></DeleteButton>
+                <ChangeButton :item="item" name="name"></ChangeButton>
+                <Reference :reference="item.response" type="responses" title="responses"></Reference>
+            </b-button-group>
+        </div>
+    </div>
+</template>
+
+<script>
+import AddButton from '../button/AddButton.vue'
+import ChangeButton from '../button/ChangeButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+import Reference from './Reference.vue'
+
+export default {
+    name: 'StatusList',
+    components: {
+        AddButton,
+        ChangeButton,
+        DeleteButton,
+        Reference,
+    },
+    props: {
+        manager: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
---
+++ src/models/Specification/Status.ts
@@ -0,0 +1,27 @@
+import KeyValue from "../Base/KeyValue"
+import UniqueItem from "../Base/UniqueItem"
+import UniqueList from "../Base/UniqueList"
+import Reference, { ReferenceType } from "./Reference"
+
+export default class Status extends UniqueItem {
+    readonly response = new Reference('OK', ReferenceType.responses)
+
+    toAPI() {
+        return this.response.toAPI()
+    }
+}
+
+export class StatusManager extends UniqueList<Status> {
+
+    constructor() {
+        super(Status)
+    }
+
+    toAPI() {
+        const map: KeyValue = {}
+        this.list.forEach(item => {
+            map[item.name] = item.toAPI()
+        })
+        return map
+    }
+}
