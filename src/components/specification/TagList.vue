99bf93b981c9374877ebcca2713027df5923cebe
GooGee <mou-gj@live.com>
2020-05-29 15:21:35 +0800

    + TagList

---
+++ src/components/specification/TagList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div>
+            <h2 class="inline mr11px">Tag</h2>
+            <b-button @click="show" variant="outline-primary"> + </b-button>
+        </div>
+        <br />
+        <div v-for="item in manager.list" :key="item.name" class="mtb11px">
+            <DeleteButton :manager="manager" :item="item">{{ item.name }}</DeleteButton>
+        </div>
+    </div>
+</template>
+
+<script>
+import builder from '../../states/builder.js'
+import dialogue from '../../states/list.js'
+import AddButton from '../button/AddButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+
+export default {
+    name: 'TagList',
+    components: {
+        AddButton,
+        DeleteButton,
+    },
+    props: {
+        manager: {
+            type: Object,
+            required: true,
+        },
+    },
+    methods: {
+        show() {
+            dialogue.showList(builder.document.tagManager.list, 'Tag', ok => {
+                try {
+                    const property = this.manager.make(dialogue.selected.name)
+                    this.manager.add(property)
+                } catch (error) {
+                    console.error(error)
+                    this.$bvToast.toast(error.message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                }
+            })
+        },
+    },
+}
+</script>
