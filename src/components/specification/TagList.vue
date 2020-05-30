6175a3c26340c20a826c3707127159c8a102afbf
GooGee <mou-gj@live.com>
2020-05-30 08:43:15 +0800

    - br in TagList

--- src/components/specification/TagList.vue
+++ src/components/specification/TagList.vue
@@ -4,7 +4,6 @@
             <h2 class="inline mr11">Tag</h2>
             <b-button @click="show" variant="outline-primary"> + </b-button>
         </div>
-        <br />
         <div v-for="item in manager.list" :key="item.name" class="mtb11">
             <DeleteButton :manager="manager" :item="item">{{ item.name }}</DeleteButton>
         </div>
