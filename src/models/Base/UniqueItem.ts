4df703a5ad071aac3d07bcea1036ac0c43579beb
GooGee <mou-gj@live.com>
2020-06-01 15:05:02 +0800

    - NameItem

--- src/models/Base/UniqueItem.ts
+++ src/models/Base/UniqueItem.ts
@@ -1,26 +1,28 @@
-import NameItem from './NameItem'
+import Item from './Item'
 import ee, { EventEnum } from '../Event'
 
-export default class UniqueItem extends NameItem {
+export default class UniqueItem extends Item {
+    protected _name: string
+
+    constructor(name: string) {
+        super()
+        this._name = name
+    }
 
     get name() {
         return this._name
     }
 
     set name(name: string) {
-        this.setName(name)
-    }
-
-    setName(name: string) {
         if (this.name === name) {
-            return false
+            return
         }
 
         const old = this.name
         ee.emit(EventEnum.BeforeNameChange, this, old, name)
         this._name = name
         ee.emit(EventEnum.AfterNameChange, this, old, name)
-        return true
+        return
     }
 
 }
