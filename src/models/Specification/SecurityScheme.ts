80e89d05b82378ff709cf9c45858fef0dde20dc4
GooGee <mou-gj@live.com>
2020-06-02 14:04:43 +0800

    + SecurityType in SecurityScheme

--- src/models/Specification/SecurityScheme.ts
+++ src/models/Specification/SecurityScheme.ts
@@ -2,16 +2,30 @@ import KeyValue from "../Base/KeyValue"
 import UniqueItem from "../Base/UniqueItem"
 import UniqueList from "../Base/UniqueList"
 import { OAuthFlows } from "./OAuthFlow"
+import { Location } from "./Parameter"
+
+export enum SecurityType {
+    apiKey = 'apiKey',
+    http = 'http',
+    oauth2 = 'oauth2',
+    openIdConnect = 'openIdConnect',
+}
 
 export default class SecurityScheme extends UniqueItem {
-    type: string = ''
+    type: SecurityType = SecurityType.http
     description: string = ''
     name2: string = ''
-    location: string = ''
+    location: Location
     scheme: string = ''
     bearerFormat: string = ''
     openIdConnectUrl: string = ''
     readonly flows = new OAuthFlows
+
+    constructor(name: string, location: Location = Location.cookie) {
+        super(name)
+        this.name2 = name
+        this.location = location
+    }
 
     toAPI() {
         return {
