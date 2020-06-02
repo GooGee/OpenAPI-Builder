8853b5bd22613bfb3733488d6cdca9195b2c1604
GooGee <mou-gj@live.com>
2020-06-02 09:07:47 +0800

    + Input in SchemaList

--- src/components/specification/SchemaList.vue
+++ src/components/specification/SchemaList.vue
@@ -6,7 +6,10 @@
         <tfoot v-if="editing">
             <tr>
                 <td>
-                    <AddButton :manager="manager" name="name"></AddButton>
+                    <b-button-group>
+                        <AddButton :manager="manager" name="name"></AddButton>
+                        <b-button @click="show" variant="outline-primary"> Input </b-button>
+                    </b-button-group>
                 </td>
                 <td></td>
             </tr>
@@ -16,6 +19,7 @@
 
 <script>
 import AddButton from '../button/AddButton.vue'
+import dialogue from '../../states/dialogue.js'
 
 export default {
     name: 'SchemaList',
@@ -31,6 +35,36 @@ export default {
         editing: {
             type: Boolean,
             required: true,
+        },
+    },
+    methods: {
+        show() {
+            dialogue.json.show('', text => {
+                try {
+                    const list = JSON.parse(text)
+                    if (Array.isArray(list)) {
+                        list.forEach(item => {
+                            let found = this.manager.find(item.name)
+                            if (found) {
+                                // ok
+                            } else {
+                                found = this.manager.make(item.name)
+                                this.manager.add(found)
+                            }
+                            found.load(item)
+                        })
+                        return
+                    }
+                    throw new Error('JSON must be an Array')
+                } catch (error) {
+                    console.error(error)
+                    this.$bvToast.toast(error.message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                }
+            })
         },
     },
 }
