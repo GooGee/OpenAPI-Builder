267c798c5cd9c77941620dff64725c081f1db9ea
GooGee <mou-gj@live.com>
2020-06-19 14:44:17 +0800

    + link tool in JSONDialogue

--- src/components/JSONDialogue.vue
+++ src/components/JSONDialogue.vue
@@ -1,5 +1,11 @@
 <template>
-    <b-modal @ok="ok" v-model="dialogue.json.visible" title="Paste JSON here" size="xl">
+    <b-modal @ok="ok" v-model="dialogue.json.visible" size="xl">
+        <template v-slot:modal-header>
+            <div>
+                Paste JSON here
+                <a target="_blank" href="https://shancarter.github.io/mr-data-converter/">Tool</a>
+            </div>
+        </template>
         <textarea v-model="dialogue.json.text" class="form-control" spellcheck="false" rows="11"></textarea>
         <div>Example: <pre>[{"name":"xxx"}]</pre></div>
     </b-modal>
