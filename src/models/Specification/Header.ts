205d1280700569b54903e9e5c0e47d20683282d2
GooGee <mou-gj@live.com>
2020-06-23 10:15:45 +0800

    * fix Header

--- src/models/Specification/Header.ts
+++ src/models/Specification/Header.ts
@@ -1,11 +1,4 @@
 import Parameter from "./Parameter"
 
 export default class Header extends Parameter {
-    toOAPI() {
-        return {
-            required: this.required,
-            description: this.description,
-            schema: this.schema.toOAPI()
-        }
-    }
 }
