bff732c39da0fca8c5187c1edebc61a7a0ae70da
GooGee <mou-gj@live.com>
2020-07-07 07:39:40 +0800

    + ItemManager

---
+++ src/components/ItemManager.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div>
+            <h2 class="inline mr11">{{ title }}</h2>
+            <b-button @click="show" variant="outline-primary"> + </b-button>
+        </div>
+        <div v-for="item in manager.list" :key="item.name" class="mtb11">
+            <b-button-group>
+                <DeleteButton :manager="manager" :item="item"></DeleteButton>
+                <b-button variant="outline-secondary"> {{ item.name }} </b-button>
+            </b-button-group>
+        </div>
+    </div>
+</template>
+
+<script>
+import dialogue from '../states/dialogue.js'
+import AddButton from './button/AddButton.vue'
+import DeleteButton from './button/DeleteButton.vue'
+
+export default {
+    name: 'ItemManager',
+    components: {
+        AddButton,
+        DeleteButton,
+    },
+    props: {
+        manager: {
+            type: Object,
+            required: true,
+        },
+        list: {
+            type: Array,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: false,
+            default: '',
+        },
+    },
+    methods: {
+        show() {
+            dialogue.list.showList(this.list, this.title, ok => {
+                try {
+                    const item = this.manager.make(dialogue.list.selected.name)
+                    this.manager.add(item)
+                } catch (error) {
+                    console.error(error)
+                    this.$bvToast.toast(error.message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                }
+            })
+        },
+    },
+}
+</script>
--- src/components/specification/TagList.vue
+++ src/components/specification/TagList.vue
@@ -1,52 +1,24 @@
 <template>
-    <div>
-        <div>
-            <h2 class="inline mr11">Tag</h2>
-            <b-button @click="show" variant="outline-primary"> + </b-button>
-        </div>
-        <div v-for="item in manager.list" :key="item.name" class="mtb11">
-            <b-button-group>
-                <DeleteButton :manager="manager" :item="item"></DeleteButton>
-                <b-button variant="outline-secondary"> {{ item.name }} </b-button>
-            </b-button-group>
-        </div>
-    </div>
+    <ItemManager :manager="manager" :list="list" title="Tag"></ItemManager>
 </template>
 
 <script>
 import builder from '../../states/builder.js'
-import dialogue from '../../states/dialogue.js'
-import AddButton from '../button/AddButton.vue'
-import DeleteButton from '../button/DeleteButton.vue'
+import ItemManager from '../ItemManager.vue'
 
 export default {
     name: 'TagList',
-    components: {
-        AddButton,
-        DeleteButton,
-    },
+    components: { ItemManager },
     props: {
         manager: {
             type: Object,
             required: true,
         },
     },
-    methods: {
-        show() {
-            dialogue.list.showList(builder.document.tagManager.list, 'Tag', ok => {
-                try {
-                    const item = this.manager.make(dialogue.list.selected.name)
-                    this.manager.add(item)
-                } catch (error) {
-                    console.error(error)
-                    this.$bvToast.toast(error.message, {
-                        title: 'i',
-                        variant: 'danger',
-                        solid: true,
-                    })
-                }
-            })
-        },
+    data() {
+        return {
+            list: builder.document.tagManager.list,
+        }
     },
 }
 </script>
