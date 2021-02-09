fbdf15ad7ff14f0fc15cf9f3d11d265ea0336dcf
GooGee <mou-gj@live.com>
2021-02-09 18:38:17 +0800

    + DeleteButton.vue

---
+++ src/view/button/DeleteButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <span @click="remove" class="btn btn-outline-danger"> - </span>
+</template>
+
+<script lang="ts">
+import UniqueItem from '@/model/Base/UniqueItem'
+import UniqueItemManager from '@/model/Base/UniqueItemManager'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        manager: {
+            type: Object as PropType<UniqueItemManager<UniqueItem>>,
+            required: true,
+        },
+        item: {
+            type: Object as PropType<UniqueItem>,
+            required: true,
+        },
+    },
+    methods: {
+        remove() {
+            if (confirm('Are you sure?')) {
+                this.manager.remove(this.item)
+            }
+        },
+    },
+})
+</script>
