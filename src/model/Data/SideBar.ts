1bce3dbc51e35894ad80083ac8d851bc30b3e000
GooGee <mou-gj@live.com>
2021-02-10 13:10:08 +0800

    + SideBar

---
+++ src/model/Data/SideBar.ts
@@ -0,0 +1,79 @@
+import { filter } from '../Service/Text'
+import UniqueItem from '../Base/UniqueItem'
+import UniqueItemManager from '../Base/UniqueItemManager'
+import Project from './Project'
+
+export default class SideBar {
+    readonly manager: UniqueItemManager<UniqueItem> | null = null
+
+    item: UniqueItem | null = null
+    keyword = ''
+    title: string
+
+    constructor(title: string, manager: UniqueItemManager<UniqueItem>) {
+        this.title = title
+        this.manager = manager
+    }
+
+    get list() {
+        if (!this.manager) {
+            return []
+        }
+
+        if (this.keyword) {
+            return filter(this.keyword, this.manager.list)
+        }
+        return this.manager.list
+    }
+}
+
+export enum SideBarEnum {
+    Example = 'Example',
+    Header = 'Header',
+    Parameter = 'Parameter',
+    Preset = 'Preset',
+    Request = 'Request',
+    Response = 'Response',
+    Schema = 'Schema',
+}
+
+export class SideBarManager {
+    readonly map = new Map<SideBarEnum, SideBar>()
+
+    bind(project: Project) {
+        this.map.set(SideBarEnum.Preset, new SideBar(SideBarEnum.Preset, project.presetManager))
+
+        this.map.set(
+            SideBarEnum.Example,
+            new SideBar(SideBarEnum.Example, project.oapi.component.exampleManager),
+        )
+        this.map.set(
+            SideBarEnum.Header,
+            new SideBar(SideBarEnum.Header, project.oapi.component.headerManager),
+        )
+        this.map.set(
+            SideBarEnum.Parameter,
+            new SideBar(SideBarEnum.Parameter, project.oapi.component.parameterManager),
+        )
+        this.map.set(
+            SideBarEnum.Request,
+            new SideBar(SideBarEnum.Request, project.oapi.component.requestBodyManager),
+        )
+        this.map.set(
+            SideBarEnum.Response,
+            new SideBar(SideBarEnum.Response, project.oapi.component.responseManager),
+        )
+        this.map.set(
+            SideBarEnum.Schema,
+            new SideBar(SideBarEnum.Schema, project.oapi.component.schemaManager),
+        )
+    }
+
+    get(name: SideBarEnum) {
+        const sb = this.map.get(name)
+        if (sb) {
+            return sb
+        }
+        throw new Error(`SideBar ${name} not found!`)
+    }
+}
---
+++ src/view/part/SideBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="pre-scrollable position-fixed" style="max-height: 90%; height: 90vh">
+        <div class="text-center mtb11">
+            <h2 class="inline mr11">{{ sss.sidebar.title }}</h2>
+            <AddButton :manager="sss.sidebar.manager"></AddButton>
+        </div>
+
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Search"
+            v-model="sss.sidebar.keyword"
+        />
+
+        <div class="list-group mt11">
+            <div
+                v-for="item in sss.sidebar.list"
+                :key="item.name"
+                @click="sss.sidebar.item = item"
+                :class="Object.is(sss.sidebar.item, item) ? 'active' : ''"
+                class="list-group-item list-group-item-action"
+            >
+                {{ item.name }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sss from '@/sss.ts'
+import AddButton from '../button/AddButton.vue'
+
+export default defineComponent({
+    components: {
+        AddButton,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+})
+</script>
