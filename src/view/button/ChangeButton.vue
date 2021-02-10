2c20718e0e75af11bc60ff3fe0f90da29ba381e1
GooGee <mou-gj@live.com>
2021-02-10 13:05:32 +0800

    + ChangeButton.vue

---
+++ src/view/button/ChangeButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <span @click="change" class="btn btn-outline-primary"> {{ item[name] }} </span>
+</template>
+
+<script lang="ts">
+import UniqueItem from '@/model/Base/UniqueItem'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        item: {
+            type: Object as PropType<UniqueItem>,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        change() {
+            const item = this.item as UniqueItem
+            const name = this.name as keyof UniqueItem
+            const text = prompt(`Please input the ${this.name}`, item[name] as string)
+            if (text) {
+                item[name as 'name'] = text
+            }
+        },
+    },
+})
+</script>
