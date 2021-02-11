244e39ab6d23ad21a520f8d1ca6511bc248760ab
GooGee <mou-gj@live.com>
2021-02-11 15:33:39 +0800

    + load in ItemManager

--- src/model/Base/ItemManager.ts
+++ src/model/Base/ItemManager.ts
@@ -1,16 +1,40 @@
 import Item from './Item'
+import KeyValue from './KeyValue'
 import Manager from './Manager'
 import Newable from './Newable'
 
 export default class ItemManager<T extends Item> extends Manager<T> {
-    readonly type: Newable<T>
+    protected readonly type: Newable<T>
 
     constructor(type: Newable<T>) {
         super()
         this.type = type
     }
 
+    load(manager: ItemManager<T>) {
+        // console.log('-- load ItemManager')
+        manager.list.forEach(item => {
+            const iii = this.make()
+            iii.load(item)
+            this.list.push(iii)
+        })
+    }
+
     make() {
         return new this.type()
+    }
+
+    toJSON() {
+        return {
+            list: this.list,
+        }
+    }
+
+    toOAPI(): KeyValue | KeyValue[] {
+        return this.toOAPIArray()
+    }
+
+    toOAPIArray(): KeyValue[] {
+        return this.list.map(item => item.toOAPI())
     }
 }
