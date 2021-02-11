6abcb3f4618f57052c65a6b370c97cebdf93d526
GooGee <mou-gj@live.com>
2021-02-11 16:59:36 +0800

    + ArrayLabel.vue

---
+++ src/view/oapi/ArrayLabel.vue
@@ -0,0 +1,35 @@
+<template>
+    <label>
+        <input
+            :checked="value"
+            @input="$emit('update:isArray', $event.target.checked)"
+            class="mr3"
+            type="checkbox"
+        />
+        <span :class="{ gray: !isArray }" class="array-label">Array</span>
+    </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        isArray: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    computed: {
+        value(): boolean {
+            return this.isArray
+        },
+    },
+})
+</script>
+
+<style>
+.array-label.gray {
+    color: lightgray;
+}
+</style>
