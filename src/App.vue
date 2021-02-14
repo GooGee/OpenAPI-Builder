103636552af1187ec5188a9abaac95ec914231d1
GooGee <mou-gj@live.com>
2021-02-14 16:04:29 +0800

    + ListDialogue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
         <div class="container-fluid" style="margin-top: 55px">
             <router-view></router-view>
         </div>
+        <ListDialogue></ListDialogue>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Bar from './view/part/Bar.vue'
+import ListDialogue from './view/part/ListDialogue.vue'
 
 export default defineComponent({
     name: 'App',
     components: {
         Bar,
+        ListDialogue,
     },
 })
 </script>
