f13e8b08c6d57ff2e544d3178afee323409edf8e
GooGee <mou-gj@live.com>
2021-02-14 16:02:51 +0800

    + Reference.vue

---
+++ src/view/oapi/Reference.vue
@@ -0,0 +1,28 @@
+<template>
+    <ReferenceShow @select="select" :type="reference.type">{{ reference.ui }}</ReferenceShow>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Reference from '@/model/OAPI/Reference'
+import UniqueItem from '@/model/Base/UniqueItem'
+import sss from '@/sss.ts'
+import ReferenceShow from './ReferenceShow.vue'
+
+export default defineComponent({
+    components: {
+        ReferenceShow,
+    },
+    props: {
+        reference: {
+            type: Object as PropType<Reference>,
+            required: true,
+        },
+    },
+    methods: {
+        select(selected: UniqueItem) {
+            this.reference.ui = selected.ui
+        },
+    },
+})
+</script>
