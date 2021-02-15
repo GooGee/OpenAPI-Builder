ffa28d8836832edf55229a57d77275dd2436a547
GooGee <mou-gj@live.com>
2021-02-15 14:21:31 +0800

    + External.vue

---
+++ src/view/info/External.vue
@@ -0,0 +1,20 @@
+<template>
+    <ED :item="sss.project.oapi.externalDocs"></ED>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sss from '@/sss.ts'
+import ED from '../oapi/External.vue'
+
+export default defineComponent({
+    components: {
+        ED,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+})
+</script>
---
+++ src/view/oapi/External.vue
@@ -0,0 +1,31 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td class="text-right" width="222px">description</td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.description" />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">url</td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.url" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+})
+</script>
