1ac9dd147c732d9c8154ab4b6d1338aaff6d57b9
GooGee <mou-gj@live.com>
2021-02-16 14:35:33 +0800

    + ButtonGroup.vue

---
+++ src/view/button/ButtonGroup.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="btn-group">
+        <span
+            @click="$emit('select', item)"
+            v-for="item in list"
+            :key="item"
+            class="btn"
+            :class="item === name ? 'btn-outline-primary' : 'btn-outline-secondary'"
+        >
+            {{ item }}
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        list: {
+            type: Array,
+            required: true,
+        },
+    },
+})
+</script>
