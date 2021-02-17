265c4473beeb2ea6533294971d1647894b021146
GooGee <mou-gj@live.com>
2021-02-17 17:06:44 +0800

    + TagList.vue

---
+++ src/view/other/TagList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ItemTable :manager="sss.sidebar.manager"></ItemTable>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sss from '@/sss.ts'
+import { SideBarEnum } from '@/model/Data/SideBar'
+import ItemTable from './ItemTable.vue'
+
+export default defineComponent({
+    components: {
+        ItemTable,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+    created() {
+        sss.show(SideBarEnum.Tag)
+    },
+})
+</script>
