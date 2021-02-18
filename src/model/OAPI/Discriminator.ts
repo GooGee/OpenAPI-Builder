f3e568c155ab2520037594c53184b2faabb79a4b
GooGee <mou-gj@live.com>
2021-02-18 17:31:45 +0800

    + Discriminator.ts

---
+++ src/model/OAPI/Discriminator.ts
@@ -0,0 +1,7 @@
+import Item from '../Base/Item'
+import { NameValueManager } from './NameValue'
+
+export default class Discriminator extends Item {
+    propertyName = ''
+    // readonly manager = new NameValueManager()
+}
