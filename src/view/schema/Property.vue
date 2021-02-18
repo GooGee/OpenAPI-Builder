5e849523fad8fdf57005e7c7974bd821236c0857
GooGee <mou-gj@live.com>
2021-02-18 15:18:08 +0800

    + schema Property.vue

---
+++ src/view/schema/Property.vue
@@ -0,0 +1,32 @@
+<template>
+    <PropertyTable>
+        <tr>
+            <td class="text-right">example</td>
+            <td>
+                <textarea
+                    v-model="sss.sidebar.item.example"
+                    class="form-control"
+                    spellcheck="false"
+                    rows="3"
+                ></textarea>
+            </td>
+        </tr>
+    </PropertyTable>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sss from '@/sss.ts'
+import PropertyTable from '../oapi/PropertyTable.vue'
+
+export default defineComponent({
+    components: {
+        PropertyTable,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+})
+</script>
