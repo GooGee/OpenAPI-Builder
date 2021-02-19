28fea689fcb09fc865e45b7e675fa7f9771b86db
GooGee <mou-gj@live.com>
2021-02-19 11:46:53 +0800

    + TagPage.vue

--- src/view/TagPage.vue
+++ src/view/TagPage.vue
@@ -1,12 +1,18 @@
 <template>
-    <ItemTable :manager="sss.sidebar.manager"></ItemTable>
+    <div class="row">
+        <div class="col-3"></div>
+
+        <div class="col-9">
+            <ItemTable :manager="sss.sidebar.manager" class="mt11"></ItemTable>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import sss from '@/sss.ts'
 import { SideBarEnum } from '@/model/Data/SideBar'
-import ItemTable from './ItemTable.vue'
+import ItemTable from './part/ItemTable.vue'
 
 export default defineComponent({
     components: {
