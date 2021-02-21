15f94ed6991e9b798b1669b2406538960d17e0ba
GooGee <mou-gj@live.com>
2021-02-21 14:07:40 +0800

    + ActionEnum.ts

---
+++ src/model/Bridge/FromJava/ActionEnum.ts
@@ -0,0 +1,13 @@
+export enum ActionEnum {
+    edit = 'edit',
+    error = 'error',
+    get = 'get',
+    load = 'load',
+    move = 'move',
+    open = 'open',
+    post = 'post',
+    read = 'read',
+    refresh = 'refresh',
+    save = 'save',
+    write = 'write',
+}
