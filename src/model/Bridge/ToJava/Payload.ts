7675af288d011f7e7a8fa5fb3ac93bc446955b72
GooGee <mou-gj@live.com>
2021-02-21 14:10:15 +0800

    + Payload.ts

---
+++ src/model/Bridge/ToJava/Payload.ts
@@ -0,0 +1,9 @@
+import { ActionEnum } from '../FromJava/ActionEnum'
+
+export default class Payload {
+    constructor(public action: ActionEnum, public key: string, public data: string) {}
+
+    static make(action: ActionEnum, key: string, data: string) {
+        return new Payload(action, key, data)
+    }
+}
