ff791b61ad6833fee78f7d7ba89e49ca3e507fb7
GooGee <mou-gj@live.com>
2021-02-25 14:42:19 +0800

    + Save.ts

---
+++ src/model/Service/Save.ts
@@ -0,0 +1,47 @@
+import { ActionEnum } from '../Bridge/FromJava/ActionEnum'
+import Response from '../Bridge/FromJava/Response'
+import Handler from '../Bridge/FromJava/Handler'
+import Project from '../Data/Project'
+import File, { OAPIFolder } from './File'
+import { StatusEnum } from '../Bridge/FromJava/StatusEnum'
+
+const BackupFolder = OAPIFolder + '/backup'
+const FileName = OAPIFolder + '/data.json'
+
+export default class Save {
+    static last = ''
+    private static readonly fake = new Response(
+        ActionEnum.write,
+        FileName,
+        '',
+        'Nothing changed',
+        StatusEnum.OK,
+    )
+
+    static run(file: File, project: Project, handler?: Handler) {
+        if (project === null) {
+            return
+        }
+        const text = JSON.stringify(project)
+        if (text === this.last) {
+            if (handler) {
+                handler(this.fake)
+            }
+            return
+        }
+
+        this.last = text
+        file.move(FileName, this.makeName())
+        file.write(FileName, text, handler)
+    }
+
+    private static makeName() {
+        const dt = new Date()
+            .toISOString()
+            .substr(0, 19)
+            .replace('T', ' ')
+            .replace(':', '.')
+            .replace(':', '.')
+        return BackupFolder + '/data.' + dt + '.json'
+    }
+}
