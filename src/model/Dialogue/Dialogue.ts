e8c643ca4413f3b690fa9b6a759e91462acbf0bc
GooGee <mou-gj@live.com>
2021-02-26 10:47:22 +0800

    - DialogueEvent.ts

--- src/model/Dialogue/Dialogue.ts
+++ src/model/Dialogue/Dialogue.ts
@@ -1,33 +1,14 @@
-import { EventEnum } from '../Event/DialogueEvent'
-import EventManager from '../Event/EventManager'
-
 export default class Dialogue {
     callback: CallableFunction | null = null
     canClose = true
     hasFooter = false
     title = ''
-    protected _visible = false
     size = 'lg'
+    visible = false
 
     show(title: string, callback: CallableFunction | null = null) {
         this.title = title
         this.callback = callback
         this.visible = true
-    }
-
-    hide() {
-        this.visible = false
-    }
-
-    get visible() {
-        return this._visible
-    }
-
-    set visible(value: boolean) {
-        if (value === this._visible) {
-            return
-        }
-        this._visible = value
-        EventManager.manager.dialogue.ee.emit(EventEnum.VisibilityChange, this)
     }
 }
--- src/model/Event/EventManager.ts
+++ src/model/Event/EventManager.ts
@@ -1,6 +1,5 @@
 import UniqueItem from '../Base/UniqueItem'
 import UniqueItemManager from '../Base/UniqueItemManager'
-import DialogueEvent from './DialogueEvent'
 import ItemDelete from './ItemDelete'
 import StateEvent from './StateEvent'
 import UIChange from './UIChange'
@@ -8,7 +7,6 @@ import UIChange from './UIChange'
 export default class EventManager {
     static readonly manager = new EventManager()
 
-    readonly dialogue = new DialogueEvent()
     readonly itemDelete = new ItemDelete<UniqueItemManager<UniqueItem>, UniqueItem>()
     readonly state = new StateEvent()
     readonly uiChange = new UIChange<UniqueItem>()
