1f9cdaa2b13e3422d1c475162c5f26f7c1354d24
GooGee <mou-gj@live.com>
2021-02-28 11:00:35 +0800

    + RunButton.vue

---
+++ src/view/button/RunButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <span @click="call" :disabled="waiting" class="btn btn-outline-primary">
+        <span v-if="waiting" class="spinner-border spinner-border-sm"></span>
+        Run
+    </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sss from '@/sss.ts'
+import Toast from '../hook/Toast'
+import Wait from '../hook/Wait'
+
+export default defineComponent({
+    props: {
+        code: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props) {
+        const ddd = Wait(() => {
+            sss.run(props.code)
+            ddd.stopWaiting()
+        })
+        return ddd
+    },
+})
+</script>
