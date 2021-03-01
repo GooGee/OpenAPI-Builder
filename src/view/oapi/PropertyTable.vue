2dac2697f861e65aec38d6cf43dd0049a2b7bce5
GooGee <mou-gj@live.com>
2021-03-01 11:04:57 +0800

    -+ required in PropertyTable.vue

--- src/view/oapi/PropertyTable.vue
+++ src/view/oapi/PropertyTable.vue
@@ -4,10 +4,7 @@
             <tr>
                 <td class="text-right w111">ui</td>
                 <td>
-                    <h5 v-if="sss.sidebar.item.required" class="inline mr11">
-                        {{ sss.sidebar.item.ui }}
-                    </h5>
-                    <div v-else class="btn-group mr11">
+                    <div class="btn-group mr11">
                         <DeleteButton
                             :manager="sss.sidebar.manager"
                             :item="sss.sidebar.item"
