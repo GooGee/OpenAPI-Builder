794c63d8dd718196753a5ecae656e528cf9b4655
GooGee <mou-gj@live.com>
2021-03-02 17:41:48 +0800

    + toOAPI in Tag.ts

--- src/model/OAPI/Tag.ts
+++ src/model/OAPI/Tag.ts
@@ -1,3 +1,4 @@
+import KeyValue from '../Base/KeyValue'
 import SideBarItem from '../Base/SideBarItem'
 import UniqueItemManager from '../Base/UniqueItemManager'
 import External from './External'
@@ -5,6 +6,16 @@ import External from './External'
 export default class Tag extends SideBarItem {
     description = ''
     // readonly externalDocs = new External
+
+    toOAPI() {
+        const result: KeyValue = {
+            name: this.ui,
+        }
+        if (this.description) {
+            result.description = this.description
+        }
+        return result
+    }
 }
 
 export class TagManager extends UniqueItemManager<Tag> {
