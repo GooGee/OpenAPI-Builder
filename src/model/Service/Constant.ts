79326a1bf6b4f4df624eaa874eea398e2277ca88
GooGee <mou-gj@live.com>
2021-03-03 11:36:48 +0800

    + Constant.ts

---
+++ src/model/Service/Constant.ts
@@ -0,0 +1,7 @@
+export default {
+    CodeForScript:
+        'function run(data) {\n    /** @type {DataForScript} */\n    const ddd = data\n}',
+    CodeForTemplate:
+        'function run(data) {\n    /** @type {DataForTemplate} */\n    const ddd = data\n    return {}\n}',
+    TemplateNotFound: 'Template Not Found',
+}
