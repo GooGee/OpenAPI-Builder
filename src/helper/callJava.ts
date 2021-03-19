87f802889caa0b9ba3d420964762f9be757a00ab
GooGee <mou-gj@live.com>
2021-03-19 11:44:46 +0800

    + callJava.ts

---
+++ src/helper/callJava.ts
@@ -0,0 +1,16 @@
+import Route from '@/model/Bridge/ToJava/Route'
+import sss from '@/sss'
+import Toast from './toast'
+import wait from '../store/wait'
+
+interface CallBack {
+    (route: Route, toast: typeof Toast): void
+}
+
+export default function callJava(callback: CallBack) {
+    const data = wait(() => {
+        callback(sss.route, Toast)
+        data.stopWaiting()
+    })
+    return data
+}
