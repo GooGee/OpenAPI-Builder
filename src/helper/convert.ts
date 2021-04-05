58e5735ff92026eca864ee01172e9201bf185efa
GooGee <mou-gj@live.com>
2021-04-05 17:31:15 +0800

    - convertToSchema in convert.ts

--- src/helper/convert.ts
+++ src/helper/convert.ts
@@ -1,34 +1,21 @@
-import SchemaSimple, { SchemaSimpleManager } from '@/model/OAPI/SchemaSimple'
 import { StringObject } from './parseCSV'
 
-export function convert<T>(data: StringObject[], map: Map<string, string>) {
-    const list: T[] = []
-    const keyxx = map.keys()
+export function convert(data: StringObject[], map: Map<string, string>) {
+    const list: StringObject[] = []
+    const keyxx = Array.from(map.keys())
     data.forEach(item => {
         const one = {} as StringObject
-        Array.from(keyxx).forEach(key => {
+        keyxx.forEach(key => {
             if (item[key]) {
                 one[map.get(key)!] = item[key]
+                if (list.includes(one)) {
+                    return
+                }
+                list.push(one)
             }
         })
     })
     return list
-}
-
-export function convertToSchema(data: StringObject[], map: Map<string, string>) {
-    const list = convert<SchemaSimple>(data, map)
-    const manager = new SchemaSimpleManager()
-    list.forEach(item => {
-        const ui = manager.make(item.ui)
-        if (item.type) {
-            ui.type = item.type
-        }
-        if (item.format) {
-            ui.format = item.format
-        }
-        manager.add(ui)
-    })
-    return manager.list
 }
 
 export interface MapObject {
