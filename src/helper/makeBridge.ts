094e47b1fe83a76b53315a4ff7484da8913b6a77
GooGee <mou-gj@live.com>
2021-04-06 15:16:29 +0800

    + Response in makeBridge.ts

--- src/helper/makeBridge.ts
+++ src/helper/makeBridge.ts
@@ -1,9 +1,11 @@
+import Response from '@/model/Bridge/FromJava/Response'
 import CEFW from '@/model/Bridge/ToJava/CEFW'
 
 export default function makeBridge(cefw: CEFW) {
     cefw.JavaBridge = {
         call(text) {
-            const json = JSON.parse(text)
+            const json = JSON.parse(text) as Response
+            console.log(json.data)
             json.data = ''
             json.status = 400
             json.message = 'Not available in browser'
