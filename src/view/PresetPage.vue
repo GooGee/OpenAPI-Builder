be2f6209608df7b09a4828994266f3f522003c0b
GooGee <mou-gj@live.com>
2021-04-06 10:55:07 +0800

    + preset Property.vue

--- src/view/PresetPage.vue
+++ src/view/PresetPage.vue
@@ -5,88 +5,50 @@
         </div>
 
         <div class="col-9">
-            <table v-if="sss.sidebar.item" class="table">
-                <caption class="caption-top">
-                    <h2 v-if="sss.sidebar.item.required">{{ sss.sidebar.item.ui }}</h2>
-                    <div v-else class="btn-group">
-                        <DeleteButton
-                            :manager="sss.sidebar.manager"
-                            :item="sss.sidebar.item"
-                            @deleted="sss.sidebar.item = null"
-                        ></DeleteButton>
-                        <ChangeButton :item="sss.sidebar.item"></ChangeButton>
-                    </div>
-                    <input
-                        type="text"
-                        class="form-control mt11"
-                        placeholder="description"
-                        v-model="sss.sidebar.item.description"
-                    />
-                </caption>
-                <thead>
-                    <tr>
-                        <th>
-                            <SortButton :manager="sss.sidebar.item.propertyManager"></SortButton>
-                            ui
-                        </th>
-                        <th>value</th>
-                        <th>tag</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in sss.sidebar.item.propertyManager.list" :key="item.ui">
-                        <td>
-                            <div class="btn-group mr11">
-                                <DeleteButton
-                                    :manager="sss.sidebar.item.propertyManager"
-                                    :item="item"
-                                ></DeleteButton>
-                                <ChangeButton :item="item"></ChangeButton>
-                            </div>
-                        </td>
-                        <td>
-                            <input type="text" class="form-control" v-model="item.value" />
-                        </td>
-                        <td>
-                            <input type="text" class="form-control" v-model="item.tag" />
-                        </td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td>
-                            <AddButton :manager="sss.sidebar.item.propertyManager"></AddButton>
-                        </td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tfoot>
-            </table>
+            <template v-if="sss.sidebar.item">
+                <h2 v-if="sss.sidebar.item.required">{{ sss.sidebar.item.ui }}</h2>
+                <div v-else class="btn-group mt11">
+                    <DeleteButton
+                        :manager="sss.sidebar.manager"
+                        :item="sss.sidebar.item"
+                        @deleted="sss.sidebar.item = null"
+                    ></DeleteButton>
+                    <ChangeButton :item="sss.sidebar.item"></ChangeButton>
+                </div>
+                <input
+                    type="text"
+                    class="form-control mt11"
+                    placeholder="description"
+                    v-model="sss.sidebar.item.description"
+                />
+            </template>
+
+            <TabBar :route="route" class="mt11"></TabBar>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import AddButton from './button/AddButton.vue'
 import ChangeButton from './button/ChangeButton.vue'
 import DeleteButton from './button/DeleteButton.vue'
-import SortButton from './button/SortButton.vue'
 import SideBar from './part/SideBar.vue'
+import TabBar from './part/TabBar.vue'
 import sss from '@/sss.ts'
 import { SideBarEnum } from '@/model/Data/SideBar'
 
 export default defineComponent({
     components: {
-        AddButton,
         ChangeButton,
         DeleteButton,
-        SortButton,
         SideBar,
+        TabBar,
     },
     data() {
         return {
             sss,
+            route: this.$router.options.routes.find(route => route.name === 'preset'),
         }
     },
     created() {
---
+++ src/view/preset/Property.vue
@@ -0,0 +1,65 @@
+<template>
+    <table v-if="sss.sidebar.item" class="table">
+        <thead>
+            <tr>
+                <th>
+                    <SortButton :manager="sss.sidebar.item.propertyManager"></SortButton>
+                    ui
+                </th>
+                <th>value</th>
+                <th>tag</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in sss.sidebar.item.propertyManager.list" :key="item.ui">
+                <td>
+                    <div class="btn-group mr11">
+                        <DeleteButton
+                            :manager="sss.sidebar.item.propertyManager"
+                            :item="item"
+                        ></DeleteButton>
+                        <ChangeButton :item="item"></ChangeButton>
+                    </div>
+                </td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.value" />
+                </td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.tag" />
+                </td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>
+                    <AddButton :manager="sss.sidebar.item.propertyManager"></AddButton>
+                </td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AddButton from '../button/AddButton.vue'
+import ChangeButton from '../button/ChangeButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+import SortButton from '../button/SortButton.vue'
+import sss from '@/sss.ts'
+
+export default defineComponent({
+    components: {
+        AddButton,
+        ChangeButton,
+        DeleteButton,
+        SortButton,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+})
+</script>
