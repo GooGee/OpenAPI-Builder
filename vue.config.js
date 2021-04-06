785c1d7daa26f40232a6dd4907f07e6d2b354355
GooGee <mou-gj@live.com>
2021-04-06 15:46:48 +0800

    + getFolder in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 process.env.VUE_APP_VERSION = require('./package.json').version
 
+function getFolder() {
+    const list = process.env.VUE_APP_VERSION.split('.')
+    const minor = parseInt(list[1]) >= 10 ? list[1] : '0'.concat(list[1])
+    return 'build' + list[0] + minor
+}
+
 module.exports = {
     lintOnSave: false,
+    outputDir: getFolder(),
+    publicPath: '',
+    productionSourceMap: false,
 }
