032525ce3480e430e643f8eb79b15fb0e7a6c534
GooGee <mou-gj@live.com>
2021-09-29 19:26:38 +0800

    + Bar.vue

---
+++ src/view/part/Bar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav v-if="ss.ready" class="navbar navbar-expand fixed-top navbar-dark bg-primary">
+        <ul class="navbar-nav mr-auto">
+            <li v-for="item in routes" :key="item.path" class="nav-item">
+                <router-link :to="item.path" active-class="active" class="nav-link">
+                    {{ item.name }}
+                </router-link>
+            </li>
+        </ul>
+
+        <ul v-if="inBrowser" class="navbar-nav ml-auto mr11">
+            <li class="nav-item">
+                <a
+                    href="https://github.com/GooGee/OpenAPI-Builder"
+                    rel="noopener"
+                    target="_blank"
+                    class="nav-link"
+                >
+                    GitHub
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { routes } from '@/router'
+import ss from '@/ss'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        inBrowser: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        return { routes, ss }
+    },
+})
+</script>
