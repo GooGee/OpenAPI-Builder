11f75c0ca57ff7899e18aba3af0200c3245da004
GooGee <mou-gj@live.com>
2021-09-29 19:22:58 +0800

    + InfoPage.vue

---
+++ src/view/InfoPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <div class="col-3"></div>
+
+        <div class="col-9">
+            <TabBar path="/info" class="mt11"></TabBar>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TabBar from './part/TabBar.vue'
+
+export default defineComponent({
+    components: {
+        TabBar,
+    },
+    setup(props, context) {
+        return {}
+    },
+})
+</script>
