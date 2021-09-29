de94f6425edd394b5b9423e293d38e1b74be0f26
GooGee <mou-gj@live.com>
2021-09-29 19:22:42 +0800

    + Info.vue

---
+++ src/view/info/Info.vue
@@ -0,0 +1,50 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td class="text-right" width="222px">title</td>
+                <td>
+                    <input type="text" class="form-control" v-model="info.title" />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">version</td>
+                <td>
+                    <input type="text" class="form-control" v-model="info.version" />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">description</td>
+                <td>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="info.description"
+                    />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">termsOfService</td>
+                <td>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="info.termsOfService"
+                    />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import ss from '@/ss'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup(props, context) {
+        const info = ss.project?.oapi.info
+        return { info }
+    },
+})
+</script>
