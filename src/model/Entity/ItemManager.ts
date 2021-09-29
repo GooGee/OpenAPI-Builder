1b4c3bc04a3ca2c9bc5f1f6229edefb8c3bfb617
GooGee <mou-gj@live.com>
2021-09-29 19:20:00 +0800

    + ItemManager.ts

---
+++ src/model/Entity/ItemManager.ts
@@ -0,0 +1,43 @@
+import Item from './Item'
+import Manager from './Manager'
+import Newable from './Newable'
+
+export default class ItemManager<T extends Item> extends Manager<T> {
+    protected readonly type: Newable<T>
+
+    constructor(type: Newable<T>) {
+        super()
+        this.type = type
+    }
+
+    load(manager: ItemManager<T>) {
+        this.loadList(manager.list)
+    }
+
+    loadList(list: T[]) {
+        // console.log('-- load ItemManager')
+        list.forEach(item => {
+            const iii = this.make()
+            iii.load(item)
+            this.list.push(iii)
+        })
+    }
+
+    make(...args: any[]) {
+        return new this.type(...args)
+    }
+
+    toJSON() {
+        return {
+            list: this.list,
+        }
+    }
+
+    toOAPI(): any {
+        return this.toOAPIArray()
+    }
+
+    toOAPIArray(): any[] {
+        return this.list.map(item => item.toOAPI())
+    }
+}
