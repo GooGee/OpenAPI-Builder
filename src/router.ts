e44d48be56a69805176ccdfddfe17826aa2b75d5
GooGee <mou-gj@live.com>
2021-09-30 17:23:27 +0800

    + info in router.ts

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,18 @@ export const routes: Array<RouteRecordRaw> = [
         name: 'home',
         component: () => import('@/view/Home.vue'),
     },
+    {
+        path: '/info',
+        name: 'info',
+        component: () => import('@/view/InfoPage.vue'),
+        children: [
+            { path: '', redirect: '/info/info' },
+            { path: 'contact', component: () => import('@/view/info/Contact.vue') },
+            { path: 'external', component: () => import('@/view/info/External.vue') },
+            { path: 'info', component: () => import('@/view/info/Info.vue') },
+            { path: 'license', component: () => import('@/view/info/License.vue') },
+        ],
+    },
 ]
 
 const router = createRouter({
