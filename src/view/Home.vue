e9532ffcded398a2c723491fbda40345b3b1d7e0
GooGee <mou-gj@live.com>
2021-09-30 14:02:40 +0800

    + logo in Home.vue

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,9 +1,39 @@
 <template>
-    <h1>Home</h1>
+    <div class="text-center" style="margin-top: 11%">
+        <img
+            v-if="store.ready"
+            alt="logo"
+            src="../asset/image/logo.svg"
+            style="width: 222px"
+        />
+        <img v-else alt="loading" src="../asset/image/loading.svg" class="mb11" />
+        <h1>{{ title }}</h1>
+        <p>{{ version }}</p>
+        <p v-if="inBrowser">
+            <span @click="create" class="btn btn-outline-primary"> New </span>
+        </p>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import ss from '@/ss'
+import store, { setReady } from '@/store'
+import { defineComponent, inject } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+    setup() {
+        const inBrowser = inject('inBrowser', true)
+        function create() {
+            ss.create()
+            setReady()
+        }
+        return {
+            create,
+            inBrowser,
+            store,
+            title: process.env.VUE_APP_TITLE,
+            version: process.env.VUE_APP_VERSION,
+        }
+    },
+})
 </script>
