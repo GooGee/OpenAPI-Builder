04187b0905bb0aa91118614077f15182870c5703
GooGee <mou-gj@live.com>
2021-09-30 17:20:46 +0800

    + External.vue

---
+++ src/view/oapi/External.vue
@@ -0,0 +1,36 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td class="text-right" width="222px">description</td>
+                <td>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="item.description"
+                    />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">url</td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.url" />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import External from '@/model/OAPI/External'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        item: {
+            type: External,
+            required: true,
+        },
+    },
+})
+</script>
