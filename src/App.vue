57d740df4f175bdac2dd96e6a625a0a9f2a98f74
GooGee <mou-gj@live.com>
2021-09-30 14:02:01 +0800

    + Bar in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link>
+    <Bar :inBrowser="inBrowser"></Bar>
+    <div class="container-fluid" style="margin-top: 55px">
+        <router-view></router-view>
     </div>
-    <router-view />
 </template>
+
+<script lang="ts">
+import Bar from '@/view/part/Bar.vue'
+import { defineComponent, provide } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+    components: {
+        Bar,
+    },
+    setup(props, context) {
+        const route = useRoute()
+        let ide = ''
+        if (route.params.ide === undefined) {
+        } else {
+            if (Array.isArray(route.params.ide)) {
+                ide = route.params.ide[0]
+            } else {
+                ide = route.params.ide
+            }
+        }
+        const inBrowser = ide === ''
+        provide('inBrowser', inBrowser)
+        return {
+            inBrowser,
+        }
+    },
+})
+</script>
