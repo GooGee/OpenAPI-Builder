c3f3c95c5487ffe60518498bdf63182fde999251
GooGee <mou-gj@live.com>
2021-10-08 17:02:50 +0800

    + ExamplePage.vue

---
+++ src/view/ExamplePage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <SideBar :sidebar="sidebar"></SideBar>
+        </div>
+
+        <div class="col-9">
+            <Example :sidebar="sidebar"></Example>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { SideBarEnum } from '@/model/Entity/SideBar'
+import ss from '@/ss'
+import { defineComponent, reactive } from 'vue'
+import Example from './oapi/Example.vue'
+import SideBar from './part/SideBar.vue'
+
+export default defineComponent({
+    components: {
+        Example,
+        SideBar,
+    },
+    setup(props, context) {
+        const sidebar = reactive(ss.sbManager.get(SideBarEnum.Example))
+        return { sidebar }
+    },
+})
+</script>
