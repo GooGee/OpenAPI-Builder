5c6df137f5380ab6939c9caa886e99d1576568ac
GooGee <mou-gj@live.com>
2021-10-08 17:01:03 +0800

    + ScriptPage.vue

---
+++ src/view/ScriptPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <SideBar :sidebar="sidebar"></SideBar>
+        </div>
+
+        <div class="col-9">
+            <Script :sidebar="sidebar"></Script>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { SideBarEnum } from '@/model/Entity/SideBar'
+import ss from '@/ss'
+import { defineComponent, reactive } from 'vue'
+import SideBar from './part/SideBar.vue'
+import Script from './table/Script.vue'
+
+export default defineComponent({
+    components: {
+        Script,
+        SideBar,
+    },
+    setup(props, context) {
+        const sidebar = reactive(ss.sbManager.get(SideBarEnum.Script))
+        return { sidebar }
+    },
+})
+</script>
