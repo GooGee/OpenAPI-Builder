12159ce9ea9e224d1854da5a625a45dfb0f840b4
GooGee <mou-gj@live.com>
2021-10-08 17:00:17 +0800

    + Preset.vue

---
+++ src/view/table/Preset.vue
@@ -0,0 +1,77 @@
+<template>
+    <table v-if="sidebar.item" class="table">
+        <caption class="caption-top">
+            <input
+                v-model="sidebar.item.description"
+                placeholder="description"
+                type="text"
+                class="form-control"
+            />
+        </caption>
+        <thead>
+            <tr>
+                <th>
+                    <SortButton :manager="sidebar.item.propertyManager"></SortButton>
+                    name
+                </th>
+                <th>value</th>
+                <th>tag</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in sidebar.item.propertyManager.list" :key="item.id">
+                <td>
+                    <div class="btn-group mr11">
+                        <DeleteButton
+                            :manager="sidebar.item.propertyManager"
+                            :item="item"
+                        ></DeleteButton>
+                        <ChangeButton
+                            :manager="sidebar.item.propertyManager"
+                            :item="item"
+                        ></ChangeButton>
+                    </div>
+                </td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.value" />
+                </td>
+                <td>
+                    <input type="text" class="form-control" v-model="item.tag" />
+                </td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>
+                    <AddButton :manager="sidebar.item.propertyManager"></AddButton>
+                </td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script lang="ts">
+import SideBar from '@/model/Entity/SideBar'
+import { defineComponent, PropType } from 'vue'
+import AddButton from '../button/AddButton.vue'
+import ChangeButton from '../button/ChangeButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+import SortButton from '../button/SortButton.vue'
+
+export default defineComponent({
+    components: {
+        AddButton,
+        ChangeButton,
+        DeleteButton,
+        SortButton,
+    },
+    props: {
+        sidebar: {
+            type: Object as PropType<SideBar>,
+            required: true,
+        },
+    },
+})
+</script>
