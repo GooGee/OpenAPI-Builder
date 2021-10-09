505b2f9428b76f4705851dac094f2a660135784b
GooGee <mou-gj@live.com>
2021-10-09 21:16:13 +0800

    + Modal.vue

---
+++ src/view/part/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <VueFinalModal v-model="value" content-class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content" style="margin-top: 66px">
+            <slot></slot>
+        </div>
+    </VueFinalModal>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+import { VueFinalModal } from 'vue-final-modal'
+
+export default {
+    components: {
+        VueFinalModal,
+    },
+    props: {
+        visible: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        const value = computed({
+            get() {
+                return props.visible
+            },
+            set(v) {
+                context.emit('update:visible', v)
+            },
+        })
+        return { value }
+    },
+}
+</script>
