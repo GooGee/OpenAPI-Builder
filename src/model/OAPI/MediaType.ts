082ff7a56e49853ee049d372500ac4dd1222a8ab
GooGee <mou-gj@live.com>
2021-10-09 21:19:29 +0800

    + MediaType.ts

---
+++ src/model/OAPI/MediaType.ts
@@ -0,0 +1,37 @@
+import KeyValue from '../Entity/KeyValue'
+import UniqueItem from '../Entity/UniqueItem'
+import UniqueItemManager from '../Entity/UniqueItemManager'
+import { EncodingManager } from './Encoding'
+import { ReferenceManager, ReferenceType } from './Reference'
+import SchemaSimple from './SchemaSimple'
+
+export enum MediaTypeEnum {
+    form = 'multipart/form-data',
+    json = 'application/json',
+    xml = 'application/xml',
+}
+
+export default class MediaType extends UniqueItem {
+    readonly schema = new SchemaSimple('')
+    readonly encodingManager = new EncodingManager()
+    readonly exampleManager = new ReferenceManager(ReferenceType.examples)
+
+    toOAPI() {
+        const result: KeyValue = {
+            schema: this.schema.toOAPI(),
+        }
+        if (this.exampleManager.list.length > 0) {
+            result.examples = this.exampleManager.toOAPI()
+        }
+        if (this.encodingManager.list.length > 0) {
+            result.encoding = this.encodingManager.toOAPI()
+        }
+        return result
+    }
+}
+
+export class MediaTypeManager extends UniqueItemManager<MediaType> {
+    constructor() {
+        super(MediaType)
+    }
+}
