0e970858b97c82b5ec8864de5a37828d47f2a71b
GooGee <mou-gj@live.com>
2021-10-09 21:23:25 +0800

    + ServerVariable.ts

---
+++ src/model/OAPI/ServerVariable.ts
@@ -0,0 +1,26 @@
+import KeyValue from '../Entity/KeyValue'
+import UniqueItem from '../Entity/UniqueItem'
+import UniqueItemManager from '../Entity/UniqueItemManager'
+
+export default class ServerVariable extends UniqueItem {
+    default = ''
+    description = ''
+    readonly valueManager = new UniqueItemManager(UniqueItem)
+
+    toOAPI() {
+        const result: KeyValue = {
+            default: this.default,
+            description: this.description,
+        }
+        if (this.valueManager.list.length) {
+            result.enum = this.valueManager.list.map(value => value.un)
+        }
+        return result
+    }
+}
+
+export class ServerVariableManager extends UniqueItemManager<ServerVariable> {
+    constructor() {
+        super(ServerVariable)
+    }
+}
