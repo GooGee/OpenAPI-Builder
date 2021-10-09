801daa1dd65aae199eca7b4f4d22546f5f28d53a
GooGee <mou-gj@live.com>
2021-10-09 21:20:42 +0800

    + Reference.ts

---
+++ src/model/OAPI/Reference.ts
@@ -0,0 +1,54 @@
+import UniqueItem from '../Entity/UniqueItem'
+import UniqueItemManager from '../Entity/UniqueItemManager'
+
+export enum ReferenceType {
+    examples = 'examples',
+    headers = 'headers',
+    links = ' links',
+    parameters = 'parameters',
+    requestBodies = 'requestBodies',
+    responses = 'responses',
+    schemas = 'schemas',
+    security = 'security',
+}
+
+export default class Reference extends UniqueItem {
+    type: ReferenceType
+
+    constructor(name: string, type: ReferenceType = ReferenceType.schemas) {
+        super(name)
+        this.type = type
+    }
+
+    get id() {
+        return this._id
+    }
+
+    set id(id: number) {
+        this._id = id
+    }
+
+    get text() {
+        return `#/components/${this.type}/${this.un}`
+    }
+
+    toOAPI() {
+        return {
+            $ref: this.text,
+        }
+    }
+}
+
+export class ReferenceManager extends UniqueItemManager<Reference> {
+    readonly referenceType: ReferenceType
+
+    constructor(type: ReferenceType) {
+        super(Reference)
+        this.referenceType = type
+    }
+
+    make(name: string, type: ReferenceType = this.referenceType) {
+        const item = new Reference(name, type)
+        return item
+    }
+}
