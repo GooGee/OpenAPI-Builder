29ca55f4986ae8f86bd541a350a22431089a567c
GooGee <mou-gj@live.com>
2021-10-09 21:20:51 +0800

    + RequestBody.ts

---
+++ src/model/OAPI/RequestBody.ts
@@ -0,0 +1,22 @@
+import SideBarItem from '../Entity/SideBarItem'
+import UniqueItemManager from '../Entity/UniqueItemManager'
+import { MediaTypeManager } from './MediaType'
+
+export default class RequestBody extends SideBarItem {
+    required = true
+    description = ''
+    readonly mediaTypeManager = new MediaTypeManager()
+
+    toOAPI() {
+        return {
+            content: this.mediaTypeManager.toOAPI(),
+            description: this.description,
+        }
+    }
+}
+
+export class RequestBodyManager extends UniqueItemManager<RequestBody> {
+    constructor() {
+        super(RequestBody)
+    }
+}
