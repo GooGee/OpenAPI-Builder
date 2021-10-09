84ed01d1d822de2330ea12d02f5da454c08d9bfe
GooGee <mou-gj@live.com>
2021-10-09 21:18:30 +0800

    + DataType.ts

---
+++ src/model/OAPI/DataType.ts
@@ -0,0 +1,27 @@
+export enum CompositionType {
+    allOf = 'allOf',
+    anyOf = 'anyOf',
+    multipleOf = 'multipleOf',
+    oneOf = 'oneOf',
+}
+
+const object = 'object'
+
+export enum SimpleType {
+    boolean = 'boolean',
+    integer = 'integer',
+    number = 'number',
+    reference = 'reference',
+    string = 'string',
+    template = 'template',
+}
+
+const DataType = { ...CompositionType, object, ...SimpleType }
+
+export default DataType
+
+export const compositionTypeList = Object.values(CompositionType)
+
+export const dataTypeList = Object.values(DataType)
+
+export const simpleTypeList = Object.values(SimpleType)
