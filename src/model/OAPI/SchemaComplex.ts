afa011f968a589f3541c147c96931a5650bd5622
GooGee <mou-gj@live.com>
2021-10-09 21:21:36 +0800

    + SchemaComplex.ts

---
+++ src/model/OAPI/SchemaComplex.ts
@@ -0,0 +1,56 @@
+import UniqueItemManager from '../Entity/UniqueItemManager'
+import Schema from './Schema'
+import SchemaComposition from './SchemaComposition'
+import SchemaObject from './SchemaObject'
+
+export enum SchemaType {
+    composition = 'composition',
+    object = 'object',
+    template = 'template',
+}
+
+export const schemaTypeList = Object.keys(SchemaType)
+
+export default class SchemaComplex extends Schema {
+    type: SchemaType = SchemaType.object
+    readonly composition = new SchemaComposition()
+    readonly object = new SchemaObject()
+
+    get empty() {
+        return (
+            this.composition.schemaManager.list.length === 0 &&
+            this.object.schemaManager.list.length === 0 &&
+            this.text === ''
+        )
+    }
+
+    get isComposition() {
+        return this.type === SchemaType.composition
+    }
+
+    get isObject() {
+        return this.type === SchemaType.object
+    }
+
+    get isTemplate() {
+        return this.type === SchemaType.template
+    }
+
+    toOAPI() {
+        if (this.isTemplate) {
+            return JSON.parse(this.text)
+        }
+
+        if (this.isObject) {
+            return this.object.toOAPI()
+        }
+
+        return this.composition.toOAPI()
+    }
+}
+
+export class SchemaManager extends UniqueItemManager<SchemaComplex> {
+    constructor() {
+        super(SchemaComplex)
+    }
+}
