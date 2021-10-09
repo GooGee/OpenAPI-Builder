b4e555c75cfcc50b2298f9453a64b18b6c6333b9
GooGee <mou-gj@live.com>
2021-10-09 21:21:20 +0800

    + Schema.ts

---
+++ src/model/OAPI/Schema.ts
@@ -0,0 +1,7 @@
+// import XML from './XML'
+import SideBarItem from '../Entity/SideBarItem';
+
+export default abstract class Schema extends SideBarItem {
+    text = ''
+    // readonly xml = new XML
+}
