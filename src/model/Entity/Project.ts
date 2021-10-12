c096678bc157d5e361d6c0a937826a1a42c0f2d7
GooGee <mou-gj@live.com>
2021-10-12 16:05:44 +0800

    + Project.ts

---
+++ src/model/Entity/Project.ts
@@ -0,0 +1,21 @@
+import Document from '../OAPI/Document'
+import Item from './Item'
+import { PresetManager } from './Preset'
+import { ScriptManager } from './Script'
+import { TemplateManager } from './Template'
+
+const Version = 1
+
+export default class Project extends Item {
+    version = Version
+
+    readonly presetManager = new PresetManager()
+    readonly scriptManager = new ScriptManager()
+    readonly templateManager = new TemplateManager()
+
+    readonly oapi = new Document()
+
+    getPreset(name: string) {
+        return this.presetManager.find(name)
+    }
+}
