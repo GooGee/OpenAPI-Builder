857d9b0b177e39dcacca82122a4216c000ced877
GooGee <mou-gj@live.com>
2021-10-12 16:07:13 +0800

    + Vendor.ts

---
+++ src/model/Vendor.ts
@@ -0,0 +1,26 @@
+import Project from './Entity/Project'
+import { SideBarManager } from './Entity/SideBar'
+import UniqueItem from './Entity/UniqueItem'
+import Text from './Helper/Text'
+
+export default class Vendor {
+    preset: Project
+    project: Project
+    readonly sbManager = new SideBarManager()
+
+    constructor(readonly data: Project) {
+        this.preset = new Project()
+        this.preset.load(data)
+        this.project = this.preset
+    }
+
+    create() {
+        this.project = new Project()
+        this.project.load(this.data)
+        this.sbManager.bind(this.project)
+    }
+
+    run(code: string, schema: UniqueItem) {
+        Text.run(code, this.project,schema)
+    }
+}
