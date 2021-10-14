1374f8e72f466ca89fb23504f2e25d5066a71fcb
GooGee <mou-gj@live.com>
2021-10-14 15:23:14 +0800

    + mitt in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import './asset/style.css'
+import mitt from 'mitt'
 import 'notyf/notyf.min.css'
+import { createApp } from 'vue'
 import App from './App.vue'
+import './asset/style.css'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).provide('emitter', mitt()).use(router).mount('#app')
