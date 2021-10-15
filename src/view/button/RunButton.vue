0a7316934f1d3a488aee2ebe29ae42ddbfc23a09
GooGee <mou-gj@live.com>
2021-10-15 17:17:17 +0800

    + RunButton.vue

---
+++ src/view/button/RunButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <span
+        @click="run"
+        :class="waiting ? 'disabled' : ''"
+        class="btn btn-outline-primary"
+    >
+        <span v-if="waiting" class="spinner-border spinner-border-sm"></span>
+        Run
+    </span>
+</template>
+
+<script lang="ts">
+import Script from '@/model/Entity/Script'
+import UniqueItem from '@/model/Entity/UniqueItem'
+import Text from '@/model/Service/Text'
+import Timer from '@/model/Service/Timer'
+import Toast from '@/model/Service/Toast'
+import ss from '@/ss'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        schema: {
+            type: Object as PropType<UniqueItem>,
+            required: true,
+        },
+        script: {
+            type: Object as PropType<Script>,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        const dd = Timer.wait(() => {
+            Text.run(props.script.code, ss.project, props.schema)
+            context.emit('run')
+            Toast.success('OK! Run ' + props.script.un)
+            dd.stopWaiting()
+        })
+        return dd
+    },
+})
+</script>
