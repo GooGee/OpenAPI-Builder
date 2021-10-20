951b6fb55b60f82f267eb20e248109cfd057a107
GooGee <mou-gj@live.com>
2021-10-20 15:59:53 +0800

    + EncodingList.vue

---
+++ src/view/oapi/EncodingList.vue
@@ -0,0 +1,98 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <td class="w111">un</td>
+                <td>contentType</td>
+                <td>style</td>
+                <td>explode</td>
+                <td>allowReserved</td>
+                <td>headers</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in manager.list" :key="item.id">
+                <td>
+                    <div class="btn-group">
+                        <DeleteButton :manager="manager" :item="item"></DeleteButton>
+                        <span class="btn btn-outline-secondary"> {{ item.un }} </span>
+                    </div>
+                </td>
+                <td>
+                    <input
+                        v-model="item.contentType"
+                        type="text"
+                        class="form-control"
+                    />
+                </td>
+                <td>
+                    <input v-model="item.style" type="text" class="form-control" />
+                </td>
+                <td>
+                    <span class="custom-control custom-switch">
+                        <input
+                            :id="item.id + 'explode'"
+                            v-model="item.explode"
+                            type="checkbox"
+                            class="custom-control-input"
+                        />
+                        <label
+                            :for="item.id + 'explode'"
+                            class="custom-control-label"
+                        ></label>
+                    </span>
+                </td>
+                <td>
+                    <span class="custom-control custom-switch">
+                        <input
+                            :id="item.id + 'allowReserved'"
+                            v-model="item.allowReserved"
+                            type="checkbox"
+                            class="custom-control-input"
+                        />
+                        <label
+                            :for="item.id + 'allowReserved'"
+                            class="custom-control-label"
+                        ></label>
+                    </span>
+                </td>
+                <td>
+                    <ReferenceList
+                        :manager="item.headerManager"
+                        type="headers"
+                    ></ReferenceList>
+                </td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>
+                    <AddButton :manager="manager"></AddButton>
+                </td>
+                <td colspan="5"></td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script lang="ts">
+import { EncodingManager } from '@/model/OAPI/Encoding'
+import { defineComponent, PropType } from 'vue'
+import AddButton from '../button/AddButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+import ReferenceList from './ReferenceList.vue'
+
+export default defineComponent({
+    components: {
+        AddButton,
+        DeleteButton,
+        ReferenceList,
+    },
+    props: {
+        manager: {
+            type: Object as PropType<EncodingManager>,
+            required: true,
+        },
+    },
+})
+</script>
