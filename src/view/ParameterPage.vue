5d3505399a761003d847a69abbc9131cd9b385ee
GooGee <mou-gj@live.com>
2021-10-20 16:01:56 +0800

    + ParameterPage.vue

---
+++ src/view/ParameterPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <SideBar :sidebar="sidebar"></SideBar>
+        </div>
+
+        <div v-if="sidebar.item" class="col-9">
+            <Parameter :sidebar="sidebar"></Parameter>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { SideBarEnum } from '@/model/Entity/SideBar'
+import ss from '@/ss'
+import { defineComponent, reactive } from 'vue'
+import Parameter from './oapi/Parameter.vue'
+import SideBar from './part/SideBar.vue'
+
+export default defineComponent({
+    components: {
+        Parameter,
+        SideBar,
+    },
+    setup(props, context) {
+        const sidebar = reactive(ss.sbManager.get(SideBarEnum.Parameter))
+        return { sidebar }
+    },
+})
+</script>
