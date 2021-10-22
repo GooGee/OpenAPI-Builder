1da8028c48ccd94d0782dd39be70de7f6d629828
GooGee <mou-gj@live.com>
2021-10-22 17:25:15 +0800

    + Content.vue

---
+++ src/view/response/Content.vue
@@ -0,0 +1,19 @@
+<template>
+    <MediaTypeList :manager="sidebar.item.mediaTypeManager"></MediaTypeList>
+</template>
+
+<script lang="ts">
+import SideBar from '@/model/Entity/SideBar'
+import { defineComponent, inject } from 'vue'
+import MediaTypeList from '../oapi/MediaTypeList.vue'
+
+export default defineComponent({
+    components: {
+        MediaTypeList,
+    },
+    setup(props, context) {
+        const sidebar = inject('sidebar') as SideBar
+        return { sidebar }
+    },
+})
+</script>
