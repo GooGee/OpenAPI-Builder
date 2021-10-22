53472e990686b2d0b23067d2fe6d021087807f31
GooGee <mou-gj@live.com>
2021-10-22 17:25:23 +0800

    + Header.vue

---
+++ src/view/response/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <ReferenceList
+        :manager="sidebar.item.headerManager"
+        type="headers"
+        class="mt11"
+    ></ReferenceList>
+</template>
+
+<script lang="ts">
+import SideBar from '@/model/Entity/SideBar'
+import { defineComponent, inject } from 'vue'
+import ReferenceList from '../oapi/ReferenceList.vue'
+
+export default defineComponent({
+    components: {
+        ReferenceList,
+    },
+    setup(props, context) {
+        const sidebar = inject('sidebar') as SideBar
+        return { sidebar }
+    },
+})
+</script>
