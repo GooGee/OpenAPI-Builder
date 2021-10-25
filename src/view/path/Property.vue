03cd4fb3c63f39623323ecb75b4b4ee9ae2f05c7
GooGee <mou-gj@live.com>
2021-10-25 15:19:44 +0800

    + Property.vue

---
+++ src/view/path/Property.vue
@@ -0,0 +1,38 @@
+<template>
+    <table class="table">
+        <tbody>
+            <tr>
+                <td class="text-right w111">description</td>
+                <td>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="sidebar.item.description"
+                    />
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">summary</td>
+                <td>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="sidebar.item.summary"
+                    />
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import SideBar from '@/model/Entity/SideBar'
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+    setup(props, context) {
+        const sidebar = inject('sidebar') as SideBar
+        return { sidebar }
+    },
+})
+</script>
