aeea6978f40a47a98eb984ce5d7e0d397603578f
GooGee <mou-gj@live.com>
2021-10-27 15:22:58 +0800

    + exclude color in SideBarItem.ts

--- src/model/Entity/SideBarItem.ts
+++ src/model/Entity/SideBarItem.ts
@@ -1,6 +1,8 @@
+import { excludeOAPI } from '../Decorator'
 import UniqueItem from './UniqueItem'
 
 export default class SideBarItem extends UniqueItem {
+    @excludeOAPI
     color = ''
     description = ''
 }
