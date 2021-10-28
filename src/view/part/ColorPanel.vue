eee264014332ae045003d4e5a1fd90435acaba08
GooGee <mou-gj@live.com>
2021-10-28 13:48:10 +0800

    + ColorPanel.vue

---
+++ src/view/part/ColorPanel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <span v-for="item in list" :key="item" @click="$emit('update:color', item)">
+            <ColorButton :color="item" :active="item === color"></ColorButton>
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ColorButton from '../button/ColorButton.vue'
+
+export default defineComponent({
+    components: {
+        ColorButton,
+    },
+    props: {
+        color: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        return {
+            list: [
+                '',
+                '#f00',
+                '#f0f',
+                '#f80',
+                '#ff0',
+                '#0f0',
+                '#0ff',
+                '#00f',
+                '#08f',
+                '#80f',
+            ],
+        }
+    },
+})
+</script>
