bdf98092d64e6b911da82d75bd3aa383de10082c
GooGee <mou-gj@live.com>
2021-11-01 14:49:27 +0800

    + loading in store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,13 @@
 import { reactive } from 'vue'
 
 const store = reactive({
+    loading: true,
     ready: false,
 })
+
+export function setLoading() {
+    store.loading = false
+}
 
 export function setReady() {
     store.ready = true
