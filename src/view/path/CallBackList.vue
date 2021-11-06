6dbb2b1b787ef692b646d4e134832d84226f849f
GooGee <mou-gj@live.com>
2021-11-06 14:27:00 +0800

    + CallBackList.vue

---
+++ src/view/path/CallBackList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div v-for="item in manager.list" :key="item.id" class="mtb11">
+        <div class="btn-group">
+            <DeleteButton :manager="manager" :item="item"></DeleteButton>
+            <ChangeButton :manager="manager" :item="item"></ChangeButton>
+            <Reference :reference="item.path" :withBlank="false"></Reference>
+        </div>
+    </div>
+    <AddButton :manager="manager"></AddButton>
+</template>
+
+<script lang="ts">
+import { CallBackManager } from '@/model/OAPI/CallBack'
+import { defineComponent, PropType } from 'vue'
+import AddButton from '../button/AddButton.vue'
+import ChangeButton from '../button/ChangeButton.vue'
+import DeleteButton from '../button/DeleteButton.vue'
+import Reference from '../oapi/Reference.vue'
+
+export default defineComponent({
+    components: {
+        AddButton,
+        ChangeButton,
+        DeleteButton,
+        Reference,
+    },
+    props: {
+        manager: {
+            type: Object as PropType<CallBackManager>,
+            required: true,
+        },
+    },
+})
+</script>
