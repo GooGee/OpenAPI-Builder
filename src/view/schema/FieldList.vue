4b967b04df12299835ed70dea0837917a936425f
GooGee <mou-gj@live.com>
2021-11-09 14:16:34 +0800

    -+ FieldList.vue

--- src/view/schema/FieldList.vue
+++ src/view/schema/FieldList.vue
@@ -35,14 +35,14 @@
             <tr>
                 <td>
                     <SelectButton
-                        @select="addName"
+                        @select="update"
                         :list="namexx"
                         :manager="manager"
                     ></SelectButton>
                 </td>
                 <td>
                     <SelectButton
-                        @select="addType"
+                        @select="update"
                         :list="typexx"
                         :manager="manager"
                     ></SelectButton>
@@ -53,8 +53,7 @@
 </template>
 
 <script lang="ts">
-import Property from '@/model/Entity/Property'
-import UniqueItem from '@/model/Entity/UniqueItem'
+import SchemaField from '@/model/OAPI/SchemaField'
 import ss from '@/ss'
 import { defineComponent, ref, watch } from 'vue'
 import ChangeButton from '../button/ChangeButton.vue'
@@ -77,46 +76,38 @@ export default defineComponent({
     },
     setup(props, context) {
         const manager = ss.project.oapi.fieldManager
-        const fieldxx = ref<UniqueItem[]>(manager.findAll(props.ui))
+        const fieldxx = ref<SchemaField[]>([])
+        const namexx = ref<SchemaField[]>([])
+        const typexx = ref<SchemaField[]>([])
+
         function update() {
             fieldxx.value = manager.findAll(props.ui)
+            namexx.value = ss.project
+                .getPreset('FieldName')!
+                .propertyManager.list.map((item) =>
+                    make(item.ui, item.un, item.tag),
+                ) as SchemaField[]
+            typexx.value = ['boolean', 'integer', 'number', 'string'].map(
+                (item, index) => make(index + 1, item, item),
+            ) as SchemaField[]
         }
-        watch(() => props.ui, update)
+        watch(() => props.ui, update, { immediate: true })
 
-        const namexx = ss.project.getPreset('FieldName')!.propertyManager.list
-        const typexx = ['boolean', 'integer', 'number', 'string'].map((item, index) => {
+        function make(ui: number, un: string, type: string) {
             return {
-                ui: index + 1,
-                un: item,
+                ui,
+                un,
+                type,
+                schemaUI: props.ui,
             }
-        })
-
-        class Schema {
-            ui = 0
-            schemaUI = 0
-            un = ''
-            type = ''
-        }
-
-        function addName(selected: Property, item: Schema) {
-            item.type = selected.tag
-            item.schemaUI = props.ui
-            update()
-        }
-
-        function addType(selected: Record<string, any>, item: Schema) {
-            item.type = item.un
-            item.schemaUI = props.ui
-            update()
         }
 
         return {
-            addName,
-            addType,
             fieldxx,
             manager,
             namexx,
             typexx,
+            update,
         }
     },
 })
