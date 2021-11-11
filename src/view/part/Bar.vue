34e7fa032d7d9e303796cf5dd5e4cf49309c2435
GooGee <mou-gj@live.com>
2021-11-11 13:46:09 +0800

    -+ route in Bar.vue

--- src/view/part/Bar.vue
+++ src/view/part/Bar.vue
@@ -4,9 +4,75 @@
         class="navbar navbar-expand fixed-top navbar-dark bg-primary"
     >
         <ul class="navbar-nav mr-auto">
-            <li v-for="item in routexx" :key="item.path" class="nav-item">
-                <router-link :to="item.path" active-class="active" class="nav-link">
-                    {{ item.name }}
+            <li class="nav-item">
+                <router-link to="/" active-class="active" class="nav-link">
+                    home
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/info" active-class="active" class="nav-link">
+                    info
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/example" active-class="active" class="nav-link">
+                    example
+                </router-link>
+            </li>
+            <li class="nav-item dropdown">
+                <span class="nav-link dropdown-toggle pointer">parameter</span>
+                <ul class="dropdown-menu">
+                    <li>
+                        <router-link
+                            to="/header"
+                            active-class="active"
+                            class="dropdown-item"
+                        >
+                            header
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link
+                            to="/parameter"
+                            active-class="active"
+                            class="dropdown-item"
+                        >
+                            parameter
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <router-link to="/path" active-class="active" class="nav-link">
+                    path
+                </router-link>
+            </li>
+            <li class="nav-item dropdown">
+                <span class="nav-link dropdown-toggle pointer">request</span>
+                <ul class="dropdown-menu">
+                    <li>
+                        <router-link
+                            to="/request"
+                            active-class="active"
+                            class="dropdown-item"
+                        >
+                            request body
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link
+                            to="/response"
+                            active-class="active"
+                            class="dropdown-item"
+                        >
+                            response
+                        </router-link>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <router-link to="/schema" active-class="active" class="nav-link">
+                    schema
                 </router-link>
             </li>
             <li class="nav-item dropdown">
@@ -31,6 +97,16 @@
                         </router-link>
                     </li>
                 </ul>
+            </li>
+            <li class="nav-item">
+                <router-link to="/server" active-class="active" class="nav-link">
+                    server
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/tag" active-class="active" class="nav-link">
+                    tag
+                </router-link>
             </li>
             <li class="nav-item dropdown">
                 <span class="nav-link dropdown-toggle pointer">preset</span>
@@ -82,7 +158,6 @@
 </template>
 
 <script lang="ts">
-import { routes } from '@/router'
 import store from '@/store'
 import { defineComponent } from 'vue'
 
@@ -94,15 +169,7 @@ export default defineComponent({
         },
     },
     setup(props, context) {
-        const excludexx = [
-            '/preset',
-            '/script',
-            '/template',
-            '/security-requirement',
-            '/security-scheme',
-        ]
-        const routexx = routes.filter((route) => !excludexx.includes(route.path))
-        return { routexx, store }
+        return { store }
     },
 })
 </script>
