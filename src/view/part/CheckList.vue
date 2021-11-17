66d9425fc602df3aeb09a36f85977ddaf8c957ed
GooGee <mou-gj@live.com>
2021-11-17 16:59:26 +0800

    + CheckList.vue

---
+++ src/view/part/CheckList.vue
@@ -0,0 +1,76 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>{{ title }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in fieldxx" :key="item.ui">
+                <td>
+                    <span class="custom-control custom-switch inline">
+                        <input
+                            :id="'checked' + item.ui"
+                            :checked="manager.has(item.ui)"
+                            @click="add(item.ui)"
+                            type="checkbox"
+                            class="custom-control-input"
+                        />
+                        <label
+                            :for="'checked' + item.ui"
+                            class="custom-control-label"
+                        ></label>
+                    </span>
+                    {{ item.un }}
+                </td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>
+                    <span @click="clear" class="btn btn-outline-danger"> Clear </span>
+                </td>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script lang="ts">
+import { UIItemManager } from '@/model/Entity/UIItem'
+import UniqueItem from '@/model/Entity/UniqueItem'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        fieldxx: {
+            type: Object as PropType<UniqueItem>,
+            required: true,
+        },
+        manager: {
+            type: Object as PropType<UIItemManager>,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props, context) {
+        function add(ui: number) {
+            const found = props.manager.find(ui)
+            if (found) {
+                props.manager.remove(found)
+            } else {
+                const item = props.manager.make(ui)
+                props.manager.add(item)
+            }
+        }
+        function clear() {
+            if (confirm('Are you sure?')) {
+                props.manager.clear()
+            }
+        }
+        return { add, clear }
+    },
+})
+</script>
--- src/view/schema/RequiredField.vue
+++ src/view/schema/RequiredField.vue
@@ -1,38 +1,10 @@
 <template>
-    <table v-if="sidebar.item.isComposition" class="table">
-        <thead>
-            <tr>
-                <th>required</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item in fieldxx" :key="item.ui">
-                <td>
-                    <span class="custom-control custom-switch inline">
-                        <input
-                            :id="'required' + item.ui"
-                            :checked="manager.has(item.ui)"
-                            @click="add(item.ui)"
-                            type="checkbox"
-                            class="custom-control-input"
-                        />
-                        <label
-                            :for="'required' + item.ui"
-                            class="custom-control-label"
-                        ></label>
-                    </span>
-                    {{ item.un }}
-                </td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td>
-                    <span @click="clear" class="btn btn-outline-danger"> Clear </span>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+    <CheckList
+        v-if="sidebar.item.isComposition"
+        :fieldxx="fieldxx"
+        :manager="manager"
+        title="required"
+    ></CheckList>
     <span v-else class="mtb11">Only for composition</span>
 </template>
 
@@ -41,29 +13,19 @@ import SideBar from '@/model/Entity/SideBar'
 import SchemaComplex from '@/model/OAPI/SchemaComplex'
 import ss from '@/ss'
 import { computed, defineComponent, inject } from 'vue'
+import CheckList from '../part/CheckList.vue'
 
 export default defineComponent({
+    components: {
+        CheckList,
+    },
     setup(props, context) {
         const sidebar = inject('sidebar') as SideBar<SchemaComplex>
         const manager = computed(() => sidebar.item!.requiredManager)
-        function add(ui: number) {
-            const found = manager.value.find(ui)
-            if (found) {
-                manager.value.remove(found)
-            } else {
-                const item = manager.value.make(ui)
-                manager.value.add(item)
-            }
-        }
-        function clear() {
-            if (confirm('Are you sure?')) {
-                manager.value.clear()
-            }
-        }
         const fieldxx = computed(() =>
             ss.project.finder.getSchemaFieldList(sidebar.item!),
         )
-        return { add, clear, fieldxx, manager, sidebar }
+        return { fieldxx, manager, sidebar }
     },
 })
 </script>
