333d4d3255cabace656ffe5d1bfa239a50e8e547
GooGee <mou-gj@live.com>
2021-11-18 15:19:56 +0800

    + ReferenceList in ServerPage.vue

--- src/view/ServerPage.vue
+++ src/view/ServerPage.vue
@@ -1,16 +1,56 @@
 <template>
     <div class="row">
-        <div class="col-6">
-            <SideBar :sidebar="sidebar"></SideBar>
-        </div>
-
-        <div v-if="sidebar.item" class="col-6">
-            <input
-                type="text"
-                class="form-control mtb11"
-                placeholder="description"
-                v-model="sidebar.item.description"
-            />
+        <div class="col-12">
+            <table class="table">
+                <caption class="caption-top">
+                    <h2>{{ sidebar.title }}</h2>
+                </caption>
+                <thead>
+                    <tr>
+                        <th>un (url)</th>
+                        <th>description</th>
+                        <th>variable</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in sidebar.manager.list" :key="item.ui">
+                        <td>
+                            <div class="btn-group">
+                                <DeleteButton
+                                    :manager="sidebar.manager"
+                                    :item="item"
+                                ></DeleteButton>
+                                <ChangeButton
+                                    :manager="sidebar.manager"
+                                    :item="item"
+                                ></ChangeButton>
+                            </div>
+                        </td>
+                        <td>
+                            <input
+                                v-model="item.description"
+                                placeholder="description"
+                                type="text"
+                                class="form-control"
+                            />
+                        </td>
+                        <td>
+                            <ReferenceList
+                                :manager="item.referenceManager"
+                            ></ReferenceList>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            <AddButton :manager="sidebar.manager"></AddButton>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tfoot>
+            </table>
         </div>
     </div>
 </template>
@@ -19,11 +59,17 @@
 import { SideBarEnum } from '@/model/Entity/SideBar'
 import ss from '@/ss'
 import { defineComponent, reactive } from 'vue'
-import SideBar from './part/SideBar.vue'
+import AddButton from './button/AddButton.vue'
+import ChangeButton from './button/ChangeButton.vue'
+import DeleteButton from './button/DeleteButton.vue'
+import ReferenceList from './oapi/ReferenceList.vue'
 
 export default defineComponent({
     components: {
-        SideBar,
+        AddButton,
+        ChangeButton,
+        DeleteButton,
+        ReferenceList,
     },
     setup(props, context) {
         const sidebar = reactive(ss.sbManager.get(SideBarEnum.Server))
