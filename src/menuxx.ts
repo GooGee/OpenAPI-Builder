a99af4f0b1b2adbb690e1fd3b21bfd9b9a1612d1
GooGee <mou-gj@live.com>
2021-11-23 14:30:43 +0800

    -+ menuxx.ts

--- src/menuxx.ts
+++ src/menuxx.ts
@@ -3,17 +3,25 @@ import Menu from './model/Entity/Menu'
 const menuxx = [
     new Menu('home', '/'),
     new Menu('info', '/info'),
-    new Menu('example', '/example'),
     new Menu('parameter', '', [
         new Menu('header', '/header'),
         new Menu('parameter', '/parameter'),
     ]),
-    new Menu('path', '/path'),
+    new Menu('path', '', [
+        //
+        new Menu('path', '/path'),
+        new Menu('tag', '/tag'),
+    ]),
     new Menu('request', '', [
+        new Menu('example', '/example'),
         new Menu('request body', '/request'),
         new Menu('response', '/response'),
     ]),
-    new Menu('schema', '/schema'),
+    new Menu('schema', '', [
+        new Menu('encoding', '/encoding'),
+        new Menu('enumer', '/enumer'),
+        new Menu('schema', '/schema'),
+    ]),
     new Menu('security', '', [
         new Menu('requirement', '/security-requirement'),
         new Menu('scheme', '/security-scheme'),
@@ -23,10 +31,7 @@ const menuxx = [
         new Menu('server', '/server'),
         new Menu('variable', '/server-variable'),
     ]),
-    new Menu('tag', '/tag'),
     new Menu('preset', '', [
-        new Menu('encoding', '/encoding'),
-        new Menu('enumer', '/enumer'),
         new Menu('property', '/preset'),
         new Menu('script', '/script'),
         // new Menu('template', '/template'),
