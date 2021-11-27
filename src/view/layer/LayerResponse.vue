59c7641b80bb9c3bbebd45b312431ba7309382f7
GooGee <mou-gj@live.com>
2021-11-27 15:03:47 +0800

    + LayerResponse.vue

---
+++ src/view/layer/LayerResponse.vue
@@ -0,0 +1,31 @@
+<template>
+    <Layer :item="item">
+        <tr>
+            <td class="text-right">LayerMediaType</td>
+            <td>
+                <ReferenceList :manager="item.mtManager"></ReferenceList>
+            </td>
+        </tr>
+    </Layer>
+</template>
+
+<script lang="ts">
+import LayerRequestBody from '@/model/Entity/LayerRequestBody'
+import LayerResponse from '@/model/Entity/LayerResponse'
+import { defineComponent, PropType } from 'vue'
+import ReferenceList from '../oapi/ReferenceList.vue'
+import Layer from './Layer.vue'
+
+export default defineComponent({
+    components: {
+        ReferenceList,
+        Layer,
+    },
+    props: {
+        item: {
+            type: Object as PropType<LayerRequestBody | LayerResponse>,
+            required: true,
+        },
+    },
+})
+</script>
