cd729eaffe9875ffac541617c591964516414328
GooGee <mou-gj@live.com>
2021-11-27 15:04:23 +0800

    + LayerPage.vue

--- src/view/LayerPage.vue
+++ src/view/LayerPage.vue
@@ -5,7 +5,23 @@
         </div>
 
         <div v-if="sidebar.item" class="col-9">
-            <Layer :item="sidebar.item"></Layer>
+            <LayerMediaType
+                v-if="type === 'LayerMediaType'"
+                :item="sidebar.item"
+            ></LayerMediaType>
+            <LayerOperation
+                v-if="type === 'LayerOperation'"
+                :item="sidebar.item"
+            ></LayerOperation>
+            <LayerPath v-if="type === 'LayerPath'" :item="sidebar.item"></LayerPath>
+            <LayerResponse
+                v-if="type === 'LayerRequestBody' || type === 'LayerResponse'"
+                :item="sidebar.item"
+            ></LayerResponse>
+            <LayerSchema
+                v-if="type === 'LayerSchema'"
+                :item="sidebar.item"
+            ></LayerSchema>
         </div>
     </div>
 </template>
@@ -15,21 +31,31 @@ import { SideBarEnum } from '@/model/Entity/SideBar'
 import ss from '@/ss'
 import { computed, defineComponent, reactive } from 'vue'
 import { useRoute } from 'vue-router'
-import Layer from './layer/Layer.vue'
+import LayerMediaType from './layer/LayerMediaType.vue'
+import LayerOperation from './layer/LayerOperation.vue'
+import LayerPath from './layer/LayerPath.vue'
+import LayerResponse from './layer/LayerResponse.vue'
+import LayerSchema from './layer/LayerSchema.vue'
 import SideBar from './part/SideBar.vue'
 
 export default defineComponent({
     components: {
-        Layer,
+        LayerMediaType,
+        LayerOperation,
+        LayerPath,
+        LayerResponse,
+        LayerSchema,
         SideBar,
     },
     setup(props, context) {
-        const sidebar = computed(function () {
+        const type = computed(function () {
             const route = useRoute()
-            const type = route.path.substring(1) as SideBarEnum
-            return reactive(ss.sbManager.get(type))
+            return route.path.substring(1) as SideBarEnum
         })
-        return { sidebar }
+        const sidebar = computed(function () {
+            return reactive(ss.sbManager.get(type.value))
+        })
+        return { sidebar, type }
     },
 })
 </script>
