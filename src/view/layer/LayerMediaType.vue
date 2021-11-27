68ee58aaa2da9c6f7325ee9790a11003b518ca8a
GooGee <mou-gj@live.com>
2021-11-27 15:02:06 +0800

    + LayerMediaType.vue

---
+++ src/view/layer/LayerMediaType.vue
@@ -0,0 +1,30 @@
+<template>
+    <Layer :item="item">
+        <tr>
+            <td class="text-right">LayerSchema</td>
+            <td>
+                <Reference :reference="item.schema"></Reference>
+            </td>
+        </tr>
+    </Layer>
+</template>
+
+<script lang="ts">
+import LayerMediaType from '@/model/Entity/LayerMediaType'
+import { defineComponent, PropType } from 'vue'
+import Reference from '../oapi/Reference.vue'
+import Layer from './Layer.vue'
+
+export default defineComponent({
+    components: {
+        Reference,
+        Layer,
+    },
+    props: {
+        item: {
+            type: Object as PropType<LayerMediaType>,
+            required: true,
+        },
+    },
+})
+</script>
