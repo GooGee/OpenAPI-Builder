0d5d9e17d91ad3cdbd56d3bab37b51688a1a084f
GooGee <mou-gj@live.com>
2021-12-01 17:41:10 +0800

    + FlowPage.vue

---
+++ src/view/FlowPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <SideBar :sidebar="sidebar"></SideBar>
+        </div>
+
+        <div v-if="sidebar.item" class="col-9">
+            <Path :item="sidebar.item"></Path>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { SideBarEnum } from '@/model/Entity/SideBar'
+import ss from '@/ss'
+import { defineComponent, reactive } from 'vue'
+import Path from './flow/Path.vue'
+import SideBar from './part/SideBar.vue'
+
+export default defineComponent({
+    components: {
+        Path,
+        SideBar,
+    },
+    setup(props, context) {
+        const sidebar = reactive(ss.sbManager.get(SideBarEnum.Flow))
+        return { sidebar }
+    },
+})
+</script>
