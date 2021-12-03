e8ab0af4a392dd4bd6150310b471e8e28b1507e8
GooGee <mou-gj@live.com>
2021-12-03 14:43:16 +0800

    -+ allowSetUI in LayerMediaType.ts

--- src/model/Entity/LayerMediaType.ts
+++ src/model/Entity/LayerMediaType.ts
@@ -3,6 +3,10 @@ import UniqueItemManager from './UniqueItemManager'
 
 export default class LayerMediaType extends UniqueItem {
     unPattern = '${schema.un}_${operation.un + path.suffix}'
+
+    protected get allowSetUI() {
+        return true
+    }
 }
 
 export class LayerMediaTypeManager extends UniqueItemManager<LayerMediaType> {
