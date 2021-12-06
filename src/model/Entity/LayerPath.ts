38431f2bdbfb479abfddd1b4dc75bb4b3a14f7ed
GooGee <mou-gj@live.com>
2021-12-06 15:04:17 +0800

    + parameterManager in LayerPath.ts

--- src/model/Entity/LayerPath.ts
+++ src/model/Entity/LayerPath.ts
@@ -1,3 +1,5 @@
+import { TargetType } from '../OAPI/Reference'
+import { LayerManager } from './Layer'
 import { LayerOperationManager } from './LayerOperation'
 import SideBarItem from './SideBarItem'
 import UniqueItemManager from './UniqueItemManager'
@@ -6,6 +8,7 @@ export default class LayerPath extends SideBarItem {
     unPattern = '/${schema.snakeCase}'
     suffix = ''
     readonly operationManager = new LayerOperationManager()
+    readonly parameterManager = new LayerManager(TargetType.parameters)
 }
 
 export class LayerPathManager extends UniqueItemManager<LayerPath> {
