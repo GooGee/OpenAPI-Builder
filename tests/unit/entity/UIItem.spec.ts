3b34f448f038ab73418806e0b45191db3d945ed9
GooGee <mou-gj@live.com>
2021-12-15 14:46:55 +0800

    + UIItem.spec.ts

---
+++ tests/unit/entity/UIItem.spec.ts
@@ -0,0 +1,25 @@
+import UIItem from '@/model/Entity/UIItem'
+
+describe('UIItem', function () {
+    const data = {
+        _ui: 1,
+    }
+
+    it('load', function () {
+        const item = new UIItem()
+        item.load(data as any)
+        expect(item.ui).toBe(data._ui)
+    })
+
+    it('throw', function () {
+        const item = new UIItem()
+        item.ui = 1
+        expect(() => (item.ui = 2)).toThrowError()
+    })
+
+    it('toJSON', function () {
+        const item = new UIItem()
+        item.load(data as any)
+        expect(item.toJSON()).toHaveProperty('_ui', data._ui)
+    })
+})
