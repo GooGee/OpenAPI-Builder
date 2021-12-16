b5e25cfd6378cf5501416af68ac1892b98094c3e
GooGee <mou-gj@live.com>
2021-12-16 09:20:28 +0800

    + TargetType.ts

---
+++ src/model/OAPI/TargetType.ts
@@ -0,0 +1,21 @@
+enum TargetType {
+    encoding = 'encoding',
+    enumer = 'enumer',
+    examples = 'examples',
+    field = 'field',
+    flow = 'flow',
+    headers = 'headers',
+    links = 'links',
+    parameters = 'parameters',
+    paths = 'paths',
+    requestBodies = 'requestBodies',
+    responses = 'responses',
+    schemas = 'schemas',
+    scope = 'scope',
+    script = 'script',
+    security = 'security',
+    tag = 'tag',
+    variable = 'variable',
+}
+
+export default TargetType
