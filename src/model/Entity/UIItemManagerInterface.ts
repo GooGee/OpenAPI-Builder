d50a7f096be5f3b936470dc5200e8f5c10bbfedd
GooGee <mou-gj@live.com>
2021-12-16 09:19:50 +0800

    + UIItemManagerInterface.ts

---
+++ src/model/Entity/UIItemManagerInterface.ts
@@ -0,0 +1,15 @@
+import ItemManagerInterface from './ItemManagerInterface'
+import UIItemInterface from './UIItemInterface'
+
+export default interface UIItemManagerInterface<
+    T extends UIItemInterface = UIItemInterface,
+> extends ItemManagerInterface<T> {
+    nextUI: number
+    add(item: T): void
+    find(ui: number): T | undefined
+    findAll(uixx: number[]): T[]
+    has(ui: number): boolean
+    load(manager: UIItemManagerInterface<T>): void
+    throwIfFind(ui: number): void
+    toUIArray(): number[]
+}
