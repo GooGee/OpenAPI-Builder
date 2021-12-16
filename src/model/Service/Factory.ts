9b308853505b30f910e3046217634aa8f267aafb
GooGee <mou-gj@live.com>
2021-12-16 09:25:48 +0800

    - makeMediaType in Factory.ts

--- src/model/Service/Factory.ts
+++ src/model/Service/Factory.ts
@@ -1,26 +1,8 @@
 import Project from '../Entity/Project'
-import { MediaTypeEnum, MediaTypeManager } from '../OAPI/MediaType'
 import SchemaComplex from '../OAPI/SchemaComplex'
 
 export default class Factory {
     constructor(readonly project: Project) {}
-
-    makeMediaType(type: MediaTypeEnum, manager: MediaTypeManager) {
-        const item = manager.make(type)
-        return item
-    }
-
-    makeMediaTypeForm(manager: MediaTypeManager) {
-        return this.makeMediaType(MediaTypeEnum.form, manager)
-    }
-
-    makeMediaTypeJSON(manager: MediaTypeManager) {
-        return this.makeMediaType(MediaTypeEnum.json, manager)
-    }
-
-    makeMediaTypeXML(manager: MediaTypeManager) {
-        return this.makeMediaType(MediaTypeEnum.xml, manager)
-    }
 
     makeSchemaField(un: string, schema: SchemaComplex) {
         const field = this.project.oapi.fieldManager.make(un)
