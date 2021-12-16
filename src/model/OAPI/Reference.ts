6e1ae322729d0ef2e8f6f4d3e3793d906e3ed63d
GooGee <mou-gj@live.com>
2021-12-16 09:20:51 +0800

    + ReferenceInterface.ts

--- src/model/OAPI/Reference.ts
+++ src/model/OAPI/Reference.ts
@@ -1,36 +1,14 @@
 import ObjectMap from '../Entity/ObjectMap'
+import ReferenceFinderInterface from '../Entity/ReferenceFinderInterface'
 import UIItem from '../Entity/UIItem'
 import UIItemManager from '../Entity/UIItemManager'
-import UniqueItem from '../Entity/UniqueItem'
-import ReferenceFinder from '../Service/ReferenceFinder'
-
-export enum TargetType {
-    encoding = 'encoding',
-    enumer = 'enumer',
-    examples = 'examples',
-    field = 'field',
-    flow = 'flow',
-    headers = 'headers',
-    links = 'links',
-    parameters = 'parameters',
-    paths = 'paths',
-    requestBodies = 'requestBodies',
-    responses = 'responses',
-    schemas = 'schemas',
-    scope = 'scope',
-    script = 'script',
-    security = 'security',
-    tag = 'tag',
-    variable = 'variable',
-}
-
-export interface OAPIReference {
-    $ref: string
-}
+import UniqueItemInterface from '../Entity/UniqueItemInterface'
+import ReferenceInterface, { OAPIReference } from './ReferenceInterface'
+import TargetType from './TargetType'
 
 export type OAPIReferenceMap = ObjectMap<OAPIReference>
 
-export default class Reference extends UIItem {
+export default class Reference extends UIItem implements ReferenceInterface {
     constructor(ui: number, public type: TargetType) {
         super()
         this.ui = ui
@@ -44,11 +22,13 @@ export default class Reference extends UIItem {
         this._ui = ui
     }
 
-    getTarget<T extends UniqueItem>(finder: ReferenceFinder) {
+    getTarget<T extends UniqueItemInterface>(
+        finder: ReferenceFinderInterface,
+    ): T | undefined {
         return finder.find<T>(this)
     }
 
-    getText<T extends UniqueItem>(source: T) {
+    getText<T extends UniqueItemInterface>(source: T) {
         if (this.type === 'paths') {
             const name = source.un.split('/').join('~1')
             return `#/${this.type}/${name}`
@@ -56,11 +36,11 @@ export default class Reference extends UIItem {
         return `#/components/${this.type}/${source.un}`
     }
 
-    toOAPI(finder: ReferenceFinder) {
+    toOAPI(finder: ReferenceFinderInterface) {
         return this.toOAPIofTarget(this.getTarget(finder))
     }
 
-    toOAPIofTarget<T extends UniqueItem>(target?: T): OAPIReference {
+    toOAPIofTarget<T extends UniqueItemInterface>(target?: T): OAPIReference {
         if (target === undefined) {
             return {
                 $ref: '??',
@@ -82,12 +62,12 @@ export class ReferenceManager extends UIItemManager<Reference> {
         this.list.push(item)
     }
 
-    getTargetxx(finder: ReferenceFinder) {
+    getTargetxx(finder: ReferenceFinderInterface) {
         if (this.list.length === 0) {
             return []
         }
 
-        const manager = finder.findManager(this.targetType) as UIItemManager<UniqueItem>
+        const manager = finder.findManager(this.targetType)
         const set = new Set(this.list.map((item) => item.ui))
         return manager.list.filter((target) => set.has(target.ui))
     }
@@ -97,7 +77,7 @@ export class ReferenceManager extends UIItemManager<Reference> {
         return item
     }
 
-    toOAPI(finder: ReferenceFinder) {
+    toOAPI(finder: ReferenceFinderInterface) {
         const targetxx = this.getTargetxx(finder)
         const map: OAPIReferenceMap = {}
         this.list.forEach((item) => {
@@ -109,7 +89,7 @@ export class ReferenceManager extends UIItemManager<Reference> {
         return map
     }
 
-    toOAPIArray(finder: ReferenceFinder) {
+    toOAPIArray(finder: ReferenceFinderInterface) {
         const targetxx = this.getTargetxx(finder)
         return this.list.map((item) => {
             const found = targetxx.find((aa) => aa.ui === item.ui)
@@ -117,7 +97,7 @@ export class ReferenceManager extends UIItemManager<Reference> {
         })
     }
 
-    toUNArray(finder: ReferenceFinder) {
+    toUNArray(finder: ReferenceFinderInterface) {
         return this.getTargetxx(finder).map((item) => item.un)
     }
 }
---
+++ src/model/OAPI/ReferenceInterface.ts
@@ -0,0 +1,19 @@
+import ReferenceFinderInterface from '../Entity/ReferenceFinderInterface'
+import UIItemInterface from '../Entity/UIItemInterface'
+import UniqueItemInterface from '../Entity/UniqueItemInterface'
+import TargetType from './TargetType'
+
+export interface OAPIReference {
+    $ref: string
+}
+
+export default interface ReferenceInterface extends UIItemInterface {
+    type: TargetType
+    ui: number
+    getTarget<T extends UniqueItemInterface>(
+        finder: ReferenceFinderInterface,
+    ): T | undefined
+    getText<T extends UniqueItemInterface>(source: T): string
+    toOAPI(finder: ReferenceFinderInterface): OAPIReference
+    toOAPIofTarget<T extends UniqueItemInterface>(target?: T): OAPIReference
+}
