5d1cafc5e91f64ea29a79e357df54376259fd86a
GooGee <mou-gj@live.com>
2021-12-18 10:32:30 +0800

    + NameReferenceManager.spec.ts

---
+++ tests/unit/oapi/NameReferenceManager.spec.ts
@@ -0,0 +1,28 @@
+import emitter from '@/emitter'
+import { NameReferenceManager } from '@/model/OAPI/NameReference'
+import TargetType from '@/model/OAPI/TargetType'
+import Start from '@/model/Service/Start'
+
+describe('NameReferenceManager', function () {
+    const ss = Start.run({} as any, emitter, window as any)
+    const schema = ss.schemaManager.make('cat')
+    ss.schemaManager.add(schema)
+
+    it('add', function () {
+        const manager = new NameReferenceManager(TargetType.schemas)
+        const item = manager.make('200')
+        item.ui = 2
+        manager.add(item)
+        expect(item.ui).toBe(1)
+        expect(() => manager.add(item)).toThrowError()
+    })
+
+    it('toOAPI', function () {
+        const manager = new NameReferenceManager(TargetType.schemas)
+        const item = manager.make('200')
+        manager.add(item)
+        item.ui = schema.ui
+        const json = manager.toOAPI(ss.finder)
+        expect(json).toHaveProperty(item.un, item.reference.toOAPI(ss.finder))
+    })
+})
