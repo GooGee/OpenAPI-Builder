de22645d8b00a4d6ad89d8eca0d05879d33c5ad5
GooGee <mou-gj@live.com>
2021-12-19 09:49:50 +0800

    -+ fix

--- src/model/Entity/UIItemManagerInterface.ts
+++ src/model/Entity/UIItemManagerInterface.ts
@@ -11,5 +11,5 @@ export default interface UIItemManagerInterface<
     has(ui: number): boolean
     load(manager: UIItemManagerInterface<T>): void
     throwIfFind(ui: number): void
-    get uixx(): number[]
+    uixx: number[]
 }
