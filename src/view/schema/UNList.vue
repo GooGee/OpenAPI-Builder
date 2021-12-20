270b65d57ba9083d845be7e0a14165786c399fce
GooGee <mou-gj@live.com>
2021-12-20 15:32:17 +0800

    + UNList.vue

---
+++ src/view/schema/UNList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-for="item in list" :key="item.ui" class="mb11">
+        {{ getUN(item.un) }}
+    </div>
+</template>
+
+<script lang="ts">
+import UniqueItemInterface from '@/model/Entity/UniqueItemInterface'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    props: {
+        getUN: {
+            type: Function,
+            required: true,
+        },
+        list: {
+            type: Object as PropType<UniqueItemInterface[]>,
+            required: true,
+        },
+    },
+})
+</script>
