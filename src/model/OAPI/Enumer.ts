7f7c48b02d934bf0519fc207104d783f8d8aed00
GooGee <mou-gj@live.com>
2021-12-20 08:54:46 +0800

    + OAPIEnumer in Enumer.ts

--- src/model/OAPI/Enumer.ts
+++ src/model/OAPI/Enumer.ts
@@ -2,11 +2,17 @@ import SideBarItem from '../Entity/SideBarItem'
 import UniqueItem from '../Entity/UniqueItem'
 import UniqueItemManager from '../Entity/UniqueItemManager'
 
+export interface OAPIEnumer {
+    type: string
+    description: string
+    enum: string[]
+}
+
 export default class Enumer extends SideBarItem {
     default = ''
     readonly valueManager = new UniqueItemManager(UniqueItem)
 
-    toOAPI() {
+    toOAPI(): OAPIEnumer {
         return {
             type: 'string',
             description: this.description,
