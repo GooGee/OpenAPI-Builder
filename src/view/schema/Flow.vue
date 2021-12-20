87106f254eed5404e5680d1a3af9cef63b810d5e
GooGee <mou-gj@live.com>
2021-12-20 15:34:51 +0800

    + Path in Flow.vue

--- src/view/schema/Flow.vue
+++ src/view/schema/Flow.vue
@@ -1,27 +1,38 @@
 <template>
     <div class="mtb11"></div>
     <span @click="run" class="btn btn-outline-primary mb11"> run </span>
-    <br />
-    <ReferenceList :manager="sidebar.item.flowManager"></ReferenceList>
+    <CheckList
+        @select="select"
+        :manager="sidebar.item!.flowManager"
+        :optionxx="optionxx"
+        title="flow"
+    ></CheckList>
+    <Path v-if="selected" :path="selected" :schema="sidebar.item!"></Path>
 </template>
 
 <script lang="ts">
 import emitter from '@/emitter'
 import { EventEnum } from '@/model/Entity/Event'
+import LayerPath from '@/model/Entity/LayerPath'
 import SideBar from '@/model/Entity/SideBar'
 import Schema from '@/model/OAPI/Schema'
 import RunFlow from '@/model/Service/RunFlow'
 import Toast from '@/model/Service/Toast'
 import ss from '@/ss'
-import { defineComponent, inject } from 'vue'
-import ReferenceList from '../oapi/ReferenceList.vue'
+import { defineComponent, inject, ref } from 'vue'
+import CheckList from '../part/CheckList.vue'
+import Path from './Path.vue'
 
 export default defineComponent({
     components: {
-        ReferenceList,
+        CheckList,
+        Path,
     },
     setup(props, context) {
         const sidebar = inject('sidebar') as SideBar<Schema>
+
+        const optionxx = ss.project.flowManager.list
+        const selected = ref<LayerPath | null>(null)
 
         function run() {
             try {
@@ -32,7 +43,17 @@ export default defineComponent({
                 Toast.error(error)
             }
         }
-        return { run, sidebar }
+
+        function select(item: LayerPath) {
+            selected.value = item
+        }
+        return {
+            optionxx,
+            run,
+            select,
+            selected,
+            sidebar,
+        }
     },
 })
 </script>
