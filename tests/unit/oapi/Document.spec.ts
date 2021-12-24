01d031f4a1e25641679af45327e3e0409fbf3ca7
GooGee <mou-gj@live.com>
2021-12-24 16:02:31 +0800

    + Document.spec.ts

---
+++ tests/unit/oapi/Document.spec.ts
@@ -0,0 +1,26 @@
+import emitter from '@/emitter'
+import Start from '@/model/Service/Start'
+
+describe('Document', function () {
+    const ss = Start.run({} as any, emitter, window as any)
+    const path = ss.pathManager.make('/user')
+    ss.pathManager.add(path)
+    const securityScheme = ss.securitySchemeManager.make('user securityScheme')
+    ss.securitySchemeManager.add(securityScheme)
+    const securityRequirement = ss.securityRequirement.make(securityScheme.ui)
+    ss.securityRequirement.add(securityRequirement)
+    const server = ss.serverManager.make('localhost')
+    ss.serverManager.add(server)
+    const tag = ss.tagManager.make('user tag')
+    ss.tagManager.add(tag)
+
+    it('toOAPI', function () {
+        const json = ss.project.oapi.toOAPI(ss.finder)
+        expect(json.info).toHaveProperty('version')
+        expect(json.components).toHaveProperty('requestBodies')
+        expect(json.paths).toHaveProperty('/user')
+        expect(json.security[0]).toHaveProperty('user securityScheme')
+        expect(json.servers[0]).toHaveProperty('url', 'localhost')
+        expect(json.tags[0]).toHaveProperty('name', 'user tag')
+    })
+})
