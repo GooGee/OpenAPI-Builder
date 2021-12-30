a3582601c9e2bb2795f0cb8d67823322da14b203
GooGee <mou-gj@live.com>
2021-12-30 16:58:08 +0800

    + PathDiagram.ts

---
+++ src/model/Diagram/PathDiagram.ts
@@ -0,0 +1,79 @@
+import { Edge, Node, Shape } from '@antv/x6'
+import LayerPath from '../Entity/LayerPath'
+import Schema from '../OAPI/Schema'
+import Text from '../Service/Text'
+
+enum RowEnum {
+    path,
+    operation,
+    response,
+    schema,
+}
+
+const RowHeight = 111
+const ColumnWidth = 222
+const ItemHeight = 33
+const ItemMargin = 11
+
+export default function make(layer: LayerPath, schema: Schema) {
+    const edges: Edge[] = []
+    const nodes: Node[] = []
+
+    const path = makeNode(
+        Text.getUN(layer.unPattern, schema, layer, layer.operation),
+        RowEnum.path,
+        0,
+    )
+    nodes.push(path)
+
+    const operation = makeNode(layer.operation.un, RowEnum.operation * RowHeight, 0)
+    nodes.push(operation)
+    edges.push(makeEdge(path, operation, 'operation'))
+
+    return {
+        edges,
+        nodes,
+    }
+}
+
+function calculateGroupHeight(length: number) {
+    return length * (ItemHeight + ItemMargin) + ItemMargin
+}
+
+function calculateWidth(label: string, width: number) {
+    let max = 0
+    label.split('\n').forEach((item) => {
+        if (max < item.length) {
+            max = item.length
+        }
+    })
+    const size = max * 9
+    if (width < size) {
+        return size
+    }
+    return width
+}
+
+function makeEdge(source: Node, target: Node, label = '') {
+    return new Shape.Edge({
+        label,
+        source,
+        target,
+    })
+}
+
+function makeNode(
+    label: string,
+    y: number,
+    x: number,
+    width: number = 111,
+    height: number = ItemHeight,
+) {
+    return new Shape.Rect({
+        x,
+        y,
+        label,
+        height,
+        width: calculateWidth(label, width),
+    })
+}
