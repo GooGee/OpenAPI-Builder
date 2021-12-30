ccf9a9d29dcb119ede7df869766c480cfb0cb5b7
GooGee <mou-gj@live.com>
2021-12-30 16:58:39 +0800

    + makeRequestBody in PathDiagram.ts

--- src/model/Diagram/PathDiagram.ts
+++ src/model/Diagram/PathDiagram.ts
@@ -1,3 +1,4 @@
+import ss from '@/ss'
 import { Edge, Node, Shape } from '@antv/x6'
 import LayerPath from '../Entity/LayerPath'
 import Schema from '../OAPI/Schema'
@@ -29,6 +30,8 @@ export default function make(layer: LayerPath, schema: Schema) {
     const operation = makeNode(layer.operation.un, RowEnum.operation * RowHeight, 0)
     nodes.push(operation)
     edges.push(makeEdge(path, operation, 'operation'))
+
+    makeRequestBody(layer, schema, edges, nodes, operation)
 
     return {
         edges,
@@ -76,4 +79,40 @@ function makeNode(
         height,
         width: calculateWidth(label, width),
     })
+}
+
+function makeRequestBody(
+    layer: LayerPath,
+    schema: Schema,
+    edges: Edge[],
+    nodes: Node[],
+    operation: Node,
+) {
+    let label = 'none'
+    if (layer.operation.withRequestBody) {
+        label = Text.getUN(
+            layer.operation.requestBody.unPattern,
+            schema,
+            layer,
+            layer.operation,
+        )
+    }
+    const rb = makeNode(label, RowEnum.response * RowHeight, 0)
+    nodes.push(rb)
+    edges.push(makeEdge(operation, rb, 'RequestBody'))
+
+    if (layer.operation.withRequestBody) {
+        const node = makeNode(
+            Text.getUN(
+                layer.operation.requestBody.schema.unPattern,
+                schema,
+                layer,
+                layer.operation,
+            ),
+            RowEnum.schema * RowHeight,
+            0,
+        )
+        nodes.push(node)
+        edges.push(makeEdge(rb, node, 'schema'))
+    }
 }
